c4ce5f4990749a8cb208f6693a2027e4addf0751
Byungil Oh <dhquddlf5@gmail.com>
2025-02-18 07:22:08 +0900

    [doc] : settings

---
+++ src/main/java/org/programmers/signalbuddyfinal/SignalBuddyFinalApplication.java
@@ -0,0 +1,13 @@
+package org.programmers.signalbuddyfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalBuddyFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignalBuddyFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/programmers/signalbuddyfinal/SignalBuddyFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.signalbuddyfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalBuddyFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
