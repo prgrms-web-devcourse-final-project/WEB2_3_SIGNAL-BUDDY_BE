ea589d968497ef914eb868aadf78de1d4d68bd86
DongminL <milkskfk12@gmail.com>
2025-02-18 22:13:31 +0900

    fix: swagger 태그 제거

--- src/main/java/org/programmers/signalbuddyfinal/domain/member/dto/MemberResponse.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/member/dto/MemberResponse.java
@@ -1,7 +1,12 @@
 package org.programmers.signalbuddyfinal.domain.member.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberRole;
 import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberStatus;
 
@@ -13,22 +18,15 @@ import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberStatus;
 @EqualsAndHashCode
 public class MemberResponse {
 
-
-    @Schema(description = "회원 ID", example = "1")
     private Long memberId;
 
-    @Schema(description = "회원 이메일", example = "test@example.com")
     private String email;
 
-    @Schema(description = "회원 닉네임", example = "TestUser")
     private String nickname;
 
-    @Schema(description = "프로필 이미지 URL", example = "http://example.com/profile.jpg")
     private String profileImageUrl;
 
-    @Schema(description = "회원 역할", example = "USER")
     private MemberRole role;
 
-    @Schema(description = "회원 상태", exampleClasses = MemberStatus.class)
     private MemberStatus memberStatus;
 }
