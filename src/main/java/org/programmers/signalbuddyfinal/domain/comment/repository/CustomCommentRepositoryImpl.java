588f6fb4d8c2b06a0e4307350be92a71719c819d
DongminL <milkskfk12@gmail.com>
2025-02-20 10:50:26 +0900

    feat: soft delete 설정

--- src/main/java/org/programmers/signalbuddyfinal/domain/comment/repository/CustomCommentRepositoryImpl.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/comment/repository/CustomCommentRepositoryImpl.java
@@ -1,10 +1,15 @@
 package org.programmers.signalbuddyfinal.domain.comment.repository;
 
+import static org.programmers.signalbuddyfinal.domain.comment.entity.QComment.comment;
+import static org.programmers.signalbuddyfinal.domain.member.entity.QMember.member;
+
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.QBean;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.programmers.signalbuddyfinal.domain.comment.dto.CommentResponse;
 import org.programmers.signalbuddyfinal.domain.member.dto.MemberResponse;
@@ -14,34 +19,33 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
-import static org.programmers.signalbuddyfinal.domain.comment.entity.QComment.comment;
-import static org.programmers.signalbuddyfinal.domain.member.entity.QMember.member;
-
 @Repository
 @RequiredArgsConstructor
 public class CustomCommentRepositoryImpl implements CustomCommentRepository {
 
     private static final QBean<MemberResponse> memberResponseDto = Projections.fields(
         MemberResponse.class, member.memberId, member.email, member.nickname,
-        member.profileImageUrl, member.role, member.memberStatus);
+        member.profileImageUrl, member.role, member.memberStatus
+    );
 
     private static final QBean<CommentResponse> commentResponseDto = Projections.fields(
         CommentResponse.class, comment.commentId, comment.content, comment.createdAt,
-        comment.updatedAt, memberResponseDto.as("member"));
+        comment.updatedAt, memberResponseDto.as("member")
+    );
 
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public Page<CommentResponse> findAllByFeedbackIdAndActiveMembers(Long feedbackId,
-        Pageable pageable) {
+    public Page<CommentResponse> findAllByFeedbackId(
+        Long feedbackId,
+        Pageable pageable
+    ) {
         List<CommentResponse> results = jpaQueryFactory
             .select(commentResponseDto).from(comment)
             .join(member).on(comment.member.eq(member)).fetchJoin()
             .where(member.memberStatus.eq(MemberStatus.ACTIVITY)
-                .and(comment.feedback.feedbackId.eq(feedbackId)))
+                .and(comment.feedback.feedbackId.eq(feedbackId))
+                .and(comment.deletedAt.isNull()))
             .offset(pageable.getOffset())
             .limit(pageable.getPageSize())
             .orderBy(new OrderSpecifier<>(Order.ASC, comment.createdAt)).fetch();
@@ -51,7 +55,8 @@ public class CustomCommentRepositoryImpl implements CustomCommentRepository {
                 .select(comment.count()).from(comment)
                 .join(member).on(comment.member.eq(member)).fetchJoin()
                 .where(member.memberStatus.eq(MemberStatus.ACTIVITY)
-                    .and(comment.feedback.feedbackId.eq(feedbackId))).fetchOne()
+                    .and(comment.feedback.feedbackId.eq(feedbackId))
+                    .and(comment.deletedAt.isNull())).fetchOne()
             ).orElse(0L);
 
         return new PageImpl<>(results, pageable, count);
