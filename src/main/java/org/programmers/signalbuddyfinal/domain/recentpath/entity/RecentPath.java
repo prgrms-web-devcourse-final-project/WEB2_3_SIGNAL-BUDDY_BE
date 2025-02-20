32360333b4e05861071cadd6d0ad57cc470290b2
dongJ <alalal2234@gmail.com>
2025-02-20 17:19:40 +0900

    fix: 패키지명 수정

--- src/main/java/org/programmers/signalbuddyfinal/domain/recentpath/entity/RecentPath.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/recentpath/entity/RecentPath.java
@@ -1,6 +1,13 @@
-package org.programmers.signalbuddyfinal.domain.recentPath.entity;
+package org.programmers.signalbuddyfinal.domain.recentpath.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,11 +33,11 @@ public class RecentPath extends BaseTimeEntity {
     private Point endPoint;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id",nullable = false)
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "bookmark_id",nullable = false)
+    @JoinColumn(name = "bookmark_id", nullable = false)
     private Bookmark bookmark;
 
 }
