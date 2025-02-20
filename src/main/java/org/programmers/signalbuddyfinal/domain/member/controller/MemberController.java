4629b7c8dce9a900b3ef3afedf2b535371d3a1ce
DongjinKim <82895809+Dongjin0224@users.noreply.github.com>
2025-02-20 12:25:55 +0900

    [Feature] 마이페이지 계정 관련 기능 추가 (#32)
    * rafactor: TrafficSignal 클래스 수정
    
    - trafficSignal -> TrafficSignal 으로 클래스명 변경
    - signal_type -> signalType 으로 필드명 변경
    
    * docs: 'data' 필드 타입 OBJECT -> VARIES 변경
    
    - 여러가지 타입이 올 수 있어서 VARIES 로 변경
    - 사용자 프로필 기본 이미지 추가
    
    * feat: 마이페이지 계정 관련 기능 추가
    
    - 계정 정보 조회, 수정, 탈퇴 기능 추가
    - 프로필 이미지 변경 및 조회 기능 추가
    - 기존 프로젝트에서 한번에 처리하던 계정 정보와 프로필 이미지 변경 로직 분리

---
+++ src/main/java/org/programmers/signalbuddyfinal/domain/member/controller/MemberController.java
@@ -0,0 +1,78 @@
+package org.programmers.signalbuddyfinal.domain.member.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.programmers.signalbuddyfinal.domain.member.dto.MemberResponse;
+import org.programmers.signalbuddyfinal.domain.member.dto.MemberUpdateRequest;
+import org.programmers.signalbuddyfinal.domain.member.service.MemberService;
+import org.programmers.signalbuddyfinal.global.response.ApiResponse;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+@Slf4j
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("{id}")
+    public ResponseEntity<ApiResponse<MemberResponse>> findById(@PathVariable Long id) {
+        final MemberResponse memberResponse = memberService.getMember(id);
+        return ResponseEntity.ok(ApiResponse.createSuccess(memberResponse));
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<ApiResponse<MemberResponse>> updateMember(@PathVariable Long id,
+        @RequestBody MemberUpdateRequest memberUpdateRequest,
+        HttpServletRequest request) {
+        log.info("id : {}, UpdateRequest: {}", id, memberUpdateRequest);
+        final MemberResponse updated = memberService.updateMember(id, memberUpdateRequest, request);
+        return ResponseEntity.ok(ApiResponse.createSuccess(updated));
+    }
+
+    @PatchMapping("{id}/profile-image")
+    public ResponseEntity<ApiResponse<String>> updateProfileImage(@PathVariable Long id,
+        @RequestPart("imageFile") MultipartFile imageFile) {
+        final String saveProfileImage = memberService.saveProfileImage(id, imageFile);
+        return ResponseEntity.ok(ApiResponse.createSuccess(saveProfileImage));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<ApiResponse<MemberResponse>> deleteMember(@PathVariable Long id) {
+        final MemberResponse deleted = memberService.deleteMember(id);
+        return ResponseEntity.ok(ApiResponse.createSuccess(deleted));
+    }
+
+    @PostMapping("{id}/verify-password")
+    public ResponseEntity<ApiResponse<Boolean>> verifyPassword(@PathVariable Long id, @RequestParam String password) {
+        final boolean verified = memberService.verifyPassword(password, id);
+        return ResponseEntity.ok(ApiResponse.createSuccess(verified));
+    }
+
+    /**
+     * ApiResponse 는 JSON 형태의 응답을 생성하는데 사용되므로 <br>
+     * 이미지 파일을 직접 반환하기 위해 {@code ResponseEntity<Resource>}를 사용.
+     *
+     * @param id Member ID
+     * @return Resource 객체
+     */
+    @GetMapping("{id}/profile-image")
+    public ResponseEntity<Resource> getImageFile(@PathVariable Long id) {
+        final Resource profileImage = memberService.getProfileImage(id);
+        return ResponseEntity.ok(profileImage);
+    }
+}
---
+++ src/test/java/org/programmers/signalbuddyfinal/domain/member/controller/MemberControllerTest.java
@@ -0,0 +1,263 @@
+package org.programmers.signalbuddyfinal.domain.member.controller;
+
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.ResourceDocumentation.headerWithName;
+import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static com.epages.restdocs.apispec.Schema.schema;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.programmers.signalbuddyfinal.global.support.RestDocsFormatGenerators.commonResponse;
+import static org.programmers.signalbuddyfinal.global.support.RestDocsFormatGenerators.commonResponseFormat;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.SimpleType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.programmers.signalbuddyfinal.domain.member.dto.MemberResponse;
+import org.programmers.signalbuddyfinal.domain.member.dto.MemberUpdateRequest;
+import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberRole;
+import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberStatus;
+import org.programmers.signalbuddyfinal.domain.member.service.MemberService;
+import org.programmers.signalbuddyfinal.global.anotation.WithMockCustomUser;
+import org.programmers.signalbuddyfinal.global.support.ControllerTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.multipart.MultipartFile;
+import org.testcontainers.shaded.org.apache.commons.lang3.ArrayUtils;
+
+@WebMvcTest(MemberController.class)
+class MemberControllerTest extends ControllerTest {
+
+    private final String tag = "Member API";
+
+    @MockitoBean
+    private MemberService memberService;
+
+    @DisplayName("ID로 유저 조회")
+    @Test
+    @WithMockCustomUser
+    void getMemberById() throws Exception {
+        final Long memberId = 1L;
+        final MemberResponse memberResponse = MemberResponse.builder().memberId(memberId)
+            .memberStatus(MemberStatus.ACTIVITY).role(MemberRole.USER).email("test@example.com")
+            .nickname("Test").build();
+
+        given(memberService.getMember(memberId)).willReturn(memberResponse);
+
+        ResultActions result = mockMvc.perform(get("/api/members/{id}", memberId));
+
+        result.andExpect(status().isOk()).andDo(
+            document("유저 정보 조회", preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()), resource(
+                    ResourceSnippetParameters.builder().tag(tag).summary("유저 정보 조회").pathParameters(
+                            parameterWithName("id").type(SimpleType.NUMBER).description("유저 ID"))
+                        .responseSchema(schema("MemberResponse")) // Schema 이름
+                        .responseFields(ArrayUtils.addAll(commonResponseFormat(),  // 공통 응답 필드 추가
+                            fieldWithPath("data.memberId").type(JsonFieldType.NUMBER)
+                                .description("유저 ID"),
+                            fieldWithPath("data.email").type(JsonFieldType.STRING)
+                                .description("유저 이메일"),
+                            fieldWithPath("data.nickname").type(JsonFieldType.STRING)
+                                .description("유저 닉네임"),
+                            fieldWithPath("data.profileImageUrl").type(JsonFieldType.STRING)
+                                .optional().description("프로필 이미지 URL"),
+                            fieldWithPath("data.role").type(JsonFieldType.STRING)
+                                .description("유저 역할"),
+                            fieldWithPath("data.memberStatus").type(JsonFieldType.STRING)
+                                .description("유저 상태"))).build())));
+    }
+
+    @DisplayName("유저 정보 수정")
+    @Test
+    @WithMockCustomUser
+    void updateMember() throws Exception {
+        // Given
+        final Long memberId = 1L;
+        final MemberUpdateRequest request = MemberUpdateRequest.builder().email("test2@example.com")
+            .nickname("Test2").password("newPassword").build();
+
+        final MemberResponse memberResponse = MemberResponse.builder().memberId(memberId)
+            .memberStatus(MemberStatus.ACTIVITY).role(MemberRole.USER).email(request.getEmail())
+            .nickname(request.getNickname()).build();
+
+        given(memberService.updateMember(eq(memberId), any(MemberUpdateRequest.class),
+            any(HttpServletRequest.class))).willReturn(memberResponse);
+
+        // When
+        ResultActions result = mockMvc.perform(patch("/api/members/{id}", memberId).contentType(
+                MediaType.APPLICATION_JSON) // JSON 요청임을 명시
+            .content(new ObjectMapper().writeValueAsString(request)) // 요청 본문 추가
+        );
+
+        // Then
+        result.andExpect(status().isOk()).andDo(
+            document("유저 정보 수정", preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()), resource(
+                    ResourceSnippetParameters.builder().tag(tag).summary("유저 정보 수정").pathParameters(
+                            parameterWithName("id").type(SimpleType.NUMBER).description("유저 ID"))
+                        .requestSchema(schema("MemberUpdateRequest")).requestFields( // 요청 필드 추가
+                            fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                            fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임"),
+                            fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"))
+                        .responseSchema(schema("MemberResponse")).responseFields(
+                            ArrayUtils.addAll(commonResponseFormat(),
+                                fieldWithPath("data.memberId").type(JsonFieldType.NUMBER)
+                                    .description("유저 ID"),
+                                fieldWithPath("data.email").type(JsonFieldType.STRING)
+                                    .description("유저 이메일"),
+                                fieldWithPath("data.nickname").type(JsonFieldType.STRING)
+                                    .description("유저 닉네임"),
+                                fieldWithPath("data.profileImageUrl").type(JsonFieldType.STRING)
+                                    .optional().description("프로필 이미지 URL"),
+                                fieldWithPath("data.role").type(JsonFieldType.STRING)
+                                    .description("유저 역할"),
+                                fieldWithPath("data.memberStatus").type(JsonFieldType.STRING)
+                                    .description("유저 상태"))).build())));
+    }
+
+    @DisplayName("유저 프로필 이미지 변경")
+    @Test
+    void updateProfileImage() throws Exception {
+        final Long memberId = 1L;
+        MockMultipartFile profileImage = new MockMultipartFile("imageFile", // 요청 필드명
+            "profile.png", // 파일명
+            MediaType.IMAGE_PNG_VALUE, new byte[]{1, 2, 3, 4} // 임의의 바이트 데이터
+        );
+
+        final String uploadedImageUrl = "https://s3.amazonaws.com/test/profile.png";
+
+        given(memberService.saveProfileImage(eq(memberId), any(MultipartFile.class))).willReturn(
+            uploadedImageUrl);
+
+        // When
+        ResultActions result = mockMvc.perform(
+            multipart(HttpMethod.PATCH, "/api/members/{id}/profile-image", memberId).file(
+                profileImage).contentType(MediaType.MULTIPART_FORM_DATA));
+
+        // TODO : Request Part 알아봐야함.
+        result.andExpect(status().isOk()).andDo(
+            document("유저 프로필 이미지 변경", preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestParts(partWithName("imageFile").description("업로드할 이미지 파일")), resource(
+                    ResourceSnippetParameters.builder().tag(tag).summary("유저 프로필 이미지 변경")
+                        .pathParameters(
+                            parameterWithName("id").type(SimpleType.NUMBER).description("유저 ID"))
+                        .responseSchema(commonResponse).responseFields(commonResponseFormat())
+                        .build())));
+    }
+
+    @DisplayName("유저 프로필 이미지 조회")
+    @Test
+    void getProfileImage() throws Exception {
+        final Long memberId = 1L;
+
+        // 가상의 이미지 데이터를 가진 ByteArrayResource 생성
+        byte[] imageData = "dummy image data".getBytes();
+        Resource imageResource = new ByteArrayResource(imageData);
+
+        given(memberService.getProfileImage(memberId)).willReturn(imageResource);
+
+        ResultActions result = mockMvc.perform(
+            get("/api/members/{id}/profile-image", memberId)
+        );
+
+        result.andExpect(status().isOk()).andDo(
+            document(
+                "유저 프로필 이미지 조회",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                resource(
+                    ResourceSnippetParameters.builder()
+                        .tag(tag)
+                        .summary("유저 프로필 이미지 조회")
+                        .pathParameters(
+                            parameterWithName("id").type(SimpleType.NUMBER).description("유저 ID")
+                        )
+                        .responseHeaders(
+                            headerWithName(HttpHeaders.CONTENT_TYPE).description("응답 콘텐츠 타입")
+                        )
+                        .build()
+                )
+            )
+        );
+    }
+
+    @DisplayName("유저 탈퇴")
+    @Test
+    void deleteMember() throws Exception {
+        final Long memberId = 1L;
+        final MemberResponse memberResponse = MemberResponse.builder().memberId(memberId)
+            .memberStatus(MemberStatus.WITHDRAWAL).role(MemberRole.USER).email("test@example.com")
+            .nickname("Test").build();
+
+        given(memberService.deleteMember(memberId)).willReturn(memberResponse);
+        ResultActions result = mockMvc.perform(delete("/api/members/{id}", memberId));
+
+        result.andExpect(status().isOk()).andDo(
+            document("유저 탈퇴", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                resource(ResourceSnippetParameters.builder().tag(tag).summary("유저 탈퇴")
+                    .pathParameters(
+                        parameterWithName("id").type(SimpleType.NUMBER).description("유저 ID"))
+                    .responseSchema(schema("MemberResponse")).responseFields(
+                        ArrayUtils.addAll(commonResponseFormat(),
+                            fieldWithPath("data.memberId").type(JsonFieldType.NUMBER)
+                                .description("유저 ID"),
+                            fieldWithPath("data.email").type(JsonFieldType.STRING)
+                                .description("유저 이메일"),
+                            fieldWithPath("data.nickname").type(JsonFieldType.STRING)
+                                .description("유저 닉네임"),
+                            fieldWithPath("data.profileImageUrl").type(JsonFieldType.STRING)
+                                .optional().description("프로필 이미지 URL"),
+                            fieldWithPath("data.role").type(JsonFieldType.STRING)
+                                .description("유저 역할"),
+                            fieldWithPath("data.memberStatus").type(JsonFieldType.STRING)
+                                .description("유저 상태"))).build())));
+    }
+
+    @DisplayName("비밀번호 확인")
+    @Test
+    void verifyPassword() throws Exception {
+        final Long memberId = 1L;
+        final String currentPassword = "currentPassword";
+
+        given(memberService.verifyPassword(currentPassword, memberId)).willReturn(true);
+
+        ResultActions result = mockMvc.perform(
+            post("/api/members/{id}/verify-password", memberId).param("password", currentPassword));
+
+        result.andExpect(status().isOk()).andDo(
+            document("현재 비밀번호 확인", preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()), resource(
+                    ResourceSnippetParameters.builder().tag(tag).summary("현재 비밀번호 확인")
+                        .pathParameters(
+                            parameterWithName("id").type(SimpleType.NUMBER).description("유저 ID"))
+                        .formParameters(parameterWithName("password").type(SimpleType.STRING)
+                            .description("현재 비밀번호")).responseSchema(commonResponse)
+                        .responseFields(commonResponseFormat()).build())));
+    }
+}
