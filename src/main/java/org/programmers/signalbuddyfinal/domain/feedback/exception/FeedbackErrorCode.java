de3696df8c91ef962eb5c701ec54b5f4a97b0037
DongjinKim <82895809+Dongjin0224@users.noreply.github.com>
2025-02-20 18:38:16 +0900

    [Refactor] ErrorCode 수정 사항 반영 (#40)
    refactor: ErrorCode 수정 사항 반영

--- src/main/java/org/programmers/signalbuddyfinal/domain/feedback/exception/FeedbackErrorCode.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/feedback/exception/FeedbackErrorCode.java
@@ -10,11 +10,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum FeedbackErrorCode implements ErrorCode {
 
-    NOT_FOUND_FEEDBACK(HttpStatus.NOT_FOUND, 40000, "해당 피드백을 찾을 수 없습니다."),
-    FEEDBACK_MODIFIER_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, 40001, "해당 게시물의 작성자와 일치하지 않습니다."),
-    FEEDBACK_ELIMINATOR_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, 40002, "해당 게시물의 작성자와 일치하지 않습니다.");
+    NOT_FOUND_FEEDBACK(HttpStatus.NOT_FOUND, "04000", "해당 피드백을 찾을 수 없습니다."),
+    FEEDBACK_MODIFIER_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "04001", "해당 게시물의 작성자와 일치하지 않습니다."),
+    FEEDBACK_ELIMINATOR_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "04002", "해당 게시물의 작성자와 일치하지 않습니다.");
 
-    private HttpStatus httpStatus;
-    private int code;
-    private String message;
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
 }
