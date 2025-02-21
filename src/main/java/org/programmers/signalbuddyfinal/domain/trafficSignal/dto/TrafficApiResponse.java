505a696634784381d5c755e9cafb9ec404fa625b
Byungil Oh <dhquddlf5@gmail.com>
2025-02-21 10:40:11 +0900

    [feat]: Traffic 세팅

---
+++ src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/dto/TrafficApiResponse.java
@@ -0,0 +1,26 @@
+package org.programmers.signalbuddyfinal.domain.trafficSignal.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.locationtech.jts.geom.Point;
+import org.programmers.signalbuddyfinal.domain.trafficSignal.entity.trafficSignal;
+import org.programmers.signalbuddyfinal.global.util.PointUtil;
+
+@Getter
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class TrafficApiResponse {
+
+    private Long serialNumber;
+    private String district;
+    private String signalType;
+    private String address;
+    private Double lat;
+    private Double lng;
+
+    public Point toPoint() { return PointUtil.toPoint(this.lat, this.lng); }
+
+}
