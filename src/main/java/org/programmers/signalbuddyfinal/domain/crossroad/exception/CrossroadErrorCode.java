3ca6aeaf7792e85feb94f9e213352a8642376ea8
DongminL <milkskfk12@gmail.com>
2025-02-21 14:36:15 +0900

    refactor: findByIdOrThrow(id)로 코드 가독성 개선

--- src/main/java/org/programmers/signalbuddyfinal/domain/crossroad/exception/CrossroadErrorCode.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/crossroad/exception/CrossroadErrorCode.java
@@ -11,7 +11,8 @@ import org.springframework.http.HttpStatus;
 public enum CrossroadErrorCode implements ErrorCode {
 
     CROSSROAD_API_REQUEST_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "03000", "교차로 API 요청이 실패했습니다."),
-    ALREADY_EXIST_CROSSROAD(HttpStatus.CONFLICT, "03001", "이미 존재하는 교차로입니다.");
+    ALREADY_EXIST_CROSSROAD(HttpStatus.CONFLICT, "03001", "이미 존재하는 교차로입니다."),
+    NOT_FOUND_CROSSROAD(HttpStatus.NOT_FOUND, "03002", "존재하지 않는 교차로입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
--- src/main/java/org/programmers/signalbuddyfinal/domain/crossroad/repository/CrossroadRepository.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/crossroad/repository/CrossroadRepository.java
@@ -1,11 +1,18 @@
 package org.programmers.signalbuddyfinal.domain.crossroad.repository;
 
 import org.programmers.signalbuddyfinal.domain.crossroad.entity.Crossroad;
+import org.programmers.signalbuddyfinal.domain.crossroad.exception.CrossroadErrorCode;
+import org.programmers.signalbuddyfinal.global.exception.BusinessException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface CrossroadRepository extends JpaRepository<Crossroad, Long>, QueryCrossroadRepository{
+public interface CrossroadRepository extends JpaRepository<Crossroad, Long>,
+    CustomCrossroadRepository {
 
+    default Crossroad findByIdOrThrow(Long id) {
+        return findById(id)
+            .orElseThrow(() -> new BusinessException(CrossroadErrorCode.NOT_FOUND_CROSSROAD));
+    }
 }
