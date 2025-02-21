dab864935bf7e214d9443ce08027eb4f558e3417
Byungil Oh <dhquddlf5@gmail.com>
2025-02-21 10:50:18 +0900

    [refactor] : Traffic 엔티티명 변경사항 적용

--- src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/dto/TrafficApiResponse.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/dto/TrafficApiResponse.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.locationtech.jts.geom.Point;
-import org.programmers.signalbuddyfinal.domain.trafficSignal.entity.trafficSignal;
+import org.programmers.signalbuddyfinal.domain.trafficSignal.entity.TrafficSignal;
 import org.programmers.signalbuddyfinal.global.util.PointUtil;
 
 @Getter
