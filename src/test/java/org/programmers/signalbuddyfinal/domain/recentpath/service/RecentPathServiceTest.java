0c053c56cc002991e1ee06722d0547a1fe855f1d
dongJ <alalal2234@gmail.com>
2025-02-21 18:43:10 +0900

    test: 최근 경로 기능 테스트

--- src/test/java/org/programmers/signalbuddyfinal/domain/recentpath/service/RecentPathServiceTest.java
+++ src/test/java/org/programmers/signalbuddyfinal/domain/recentpath/service/RecentPathServiceTest.java
@@ -1,19 +1,29 @@
 package org.programmers.signalbuddyfinal.domain.recentpath.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.Point;
 import org.programmers.signalbuddyfinal.domain.member.entity.Member;
 import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberRole;
 import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberStatus;
 import org.programmers.signalbuddyfinal.domain.member.repository.MemberRepository;
 import org.programmers.signalbuddyfinal.domain.recentpath.dto.RecentPathRequest;
 import org.programmers.signalbuddyfinal.domain.recentpath.dto.RecentPathResponse;
+import org.programmers.signalbuddyfinal.domain.recentpath.entity.RecentPath;
+import org.programmers.signalbuddyfinal.domain.recentpath.repository.RecentPathRepository;
 import org.programmers.signalbuddyfinal.global.support.ServiceTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -24,6 +34,9 @@ class RecentPathServiceTest extends ServiceTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @MockitoBean
+    private RecentPathRepository recentPathRepository;
 
     private Member member;
 
@@ -63,5 +76,30 @@ class RecentPathServiceTest extends ServiceTest {
         assertThat(recentPathList).isNotEmpty().allSatisfy(recentPathResponse -> {
             assertThat(recentPathResponse.getLastAccessedAt()).isNotNull();
         });
+    }
+
+    @DisplayName("최근 경로 방문 시간 갱신")
+    @Test
+    void updateRecentPathTime() {
+        LocalDateTime now = LocalDateTime.now().minusSeconds(1);;
+        final RecentPathResponse response = recentPathService.updateRecentPathTime(1L);
+        assertThat(response).isNotNull();
+        assertThat(response.getLastAccessedAt()).isNotNull();
+        assertThat(response.getLastAccessedAt()).isAfter(now);
+    }
+
+    @DisplayName("최근 경로와 북마크 연관관계 해제")
+    @Test
+    void unlinkBookmark() {
+        RecentPath realRecentPath = new RecentPath("테스트 경로", mock(Point.class), member);
+        RecentPath recentPath = spy(realRecentPath);
+        when(recentPathRepository.findById(1L)).thenReturn(Optional.of(recentPath));
+
+        // When
+        recentPathService.unlinkBookmark(1L);
+
+        // Then
+        verify(recentPath).unlinkBookmark();
+        assertThat(recentPath.getBookmark()).isNull();
     }
 }
