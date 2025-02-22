97fa7933afd6c39e54e706d0aef9fa3f92e61d67
DongminL <milkskfk12@gmail.com>
2025-02-22 23:48:25 +0900

    test: 다양한 조건으로 피드백 목록 조회하는 테스트

--- src/test/java/org/programmers/signalbuddyfinal/domain/feedback/repository/FeedbackRepositoryImplTest.java
+++ src/test/java/org/programmers/signalbuddyfinal/domain/feedback/repository/FeedbackRepositoryImplTest.java
@@ -3,6 +3,8 @@ package org.programmers.signalbuddyfinal.domain.feedback.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
+import java.util.Set;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,7 @@ import org.programmers.signalbuddyfinal.domain.crossroad.entity.Crossroad;
 import org.programmers.signalbuddyfinal.domain.crossroad.repository.CrossroadRepository;
 import org.programmers.signalbuddyfinal.domain.feedback.dto.FeedbackResponse;
 import org.programmers.signalbuddyfinal.domain.feedback.entity.Feedback;
+import org.programmers.signalbuddyfinal.domain.feedback.entity.enums.AnswerStatus;
 import org.programmers.signalbuddyfinal.domain.feedback.entity.enums.FeedbackCategory;
 import org.programmers.signalbuddyfinal.domain.member.entity.Member;
 import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberRole;
@@ -49,6 +52,109 @@ class FeedbackRepositoryImplTest extends RepositoryTest {
         saveFeedback("test subject4", "test content4", member, crossroad);
     }
 
+    @DisplayName("활동 중인 회원들의 피드백 목록을 조회한다.")
+    @Test
+    void getFeedbacks() {
+        final Page<FeedbackResponse> allByActiveMembers = feedbackRepository.findAllByActiveMembers(
+            Pageable.ofSize(10), AnswerStatus.BEFORE, null, null);
+
+        assertThat(allByActiveMembers).isNotNull();
+        assertThat(allByActiveMembers.getTotalElements()).isEqualTo(3);
+        assertThat(allByActiveMembers.getTotalPages()).isEqualTo(1);
+
+        assertThat(allByActiveMembers.getContent()).isNotEmpty().allSatisfy(
+            feedback -> assertThat(feedback.getMember().getMemberStatus()).isEqualTo(
+                MemberStatus.ACTIVITY));
+    }
+
+    @DisplayName("특정 교차로의 피드백 목록을 조회한다.")
+    @Test
+    void findAllByActiveMembersByCrossroadId() {
+        // Given
+        Crossroad crossroad = saveCrossroad("114111", "00삼거리", 37.42222, 127.42132);
+        String subject = "test subject";
+        String content = "test content";
+        for (int i = 10; i < 13; i++) {
+            saveFeedback(subject + i, content + i, member, crossroad);
+        }
+        saveFeedback(subject + 13, content + 13, withDrawalMember, crossroad);
+
+        // When
+        Page<FeedbackResponse> actual = feedbackRepository.findAllByActiveMembers(
+            Pageable.ofSize(10), AnswerStatus.BEFORE,
+            null, crossroad.getCrossroadId()
+        );
+
+        // Then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(actual).isNotNull();
+            softAssertions.assertThat(actual.getTotalElements()).isEqualTo(3);
+            softAssertions.assertThat(actual.getTotalPages()).isEqualTo(1);
+
+            softAssertions.assertThat(actual.getContent()).isNotEmpty().allSatisfy(
+                feedback -> assertThat(feedback.getMember().getMemberStatus())
+                    .isEqualTo(MemberStatus.ACTIVITY)
+            );
+            softAssertions.assertThat(actual.getContent().get(1).getSubject()).isEqualTo(subject + 11);
+        });
+    }
+
+    @DisplayName("특정 피드백 유형의 목록을 조회한다.")
+    @Test
+    void findAllByActiveMembersByFeedbackCategories() {
+        // Given
+        Set<FeedbackCategory> categories = Set.of(FeedbackCategory.DELAY, FeedbackCategory.ADD_SIGNAL);
+        Crossroad crossroad = saveCrossroad("114111", "00삼거리", 37.42222, 127.42132);
+        saveFeedback("test subject11", "test content11", FeedbackCategory.DELAY, member, crossroad);
+        saveFeedback("test subject12", "test content12", FeedbackCategory.DELAY, member, crossroad);
+        saveFeedback("test subject13", "test content13", FeedbackCategory.ADD_SIGNAL, member, crossroad);
+        saveFeedback("test subject14", "test content14", member, crossroad);
+
+        // When
+        Page<FeedbackResponse> actual = feedbackRepository.findAllByActiveMembers(
+            Pageable.ofSize(10), null,
+            categories, null
+        );
+
+        // Then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(actual).isNotNull();
+            softAssertions.assertThat(actual.getTotalElements()).isEqualTo(3);
+            softAssertions.assertThat(actual.getTotalPages()).isEqualTo(1);
+
+            softAssertions.assertThat(actual.getContent()).isNotEmpty().allSatisfy(
+                feedback -> assertThat(feedback.getMember().getMemberStatus())
+                    .isEqualTo(MemberStatus.ACTIVITY)
+            );
+            for (FeedbackResponse feedback : actual.getContent()) {
+                softAssertions.assertThat(feedback.getCategory())
+                    .isNotEqualTo(FeedbackCategory.ETC);
+            }
+        });
+    }
+
+    @DisplayName("활동 중인 회원들의 날짜 조건을 추가한 피드백 목록을 조회한다.")
+    @Test
+    void getFeedbacksWithDate() {
+        // 정렬 기준: "createdAt" 컬럼 기준 내림차순 (DESC)
+        Sort sort = Sort.by(Sort.Order.desc("createdAt"));
+
+        // 페이지 번호 0, 사이즈 10, 정렬 포함
+        Pageable pageable = PageRequest.of(0, 10, sort);
+        final Page<FeedbackResponse> page = feedbackRepository.findAll(pageable, LocalDate.MIN, LocalDate.MAX, AnswerStatus.BEFORE);
+
+        assertThat(page).isNotNull();
+//        assertThat(page.getTotalElements()).isEqualTo(3);
+        // TODO : 조회 결과 0으로 출력 추후 수정 필요
+        assertThat(page.getTotalElements()).isZero();
+        assertThat(page.getTotalPages()).isZero();
+
+//        assertThat(page.getContent()).isNotEmpty().allSatisfy(
+//            feedback -> assertThat(feedback.getMember().getMemberStatus()).isEqualTo(
+//                MemberStatus.ACTIVITY));
+    }
+
+
     private Member saveMember(String email, String nickname) {
         return memberRepository.save(
             Member.builder().email(email).password("123456").role(MemberRole.USER)
@@ -75,40 +181,12 @@ class FeedbackRepositoryImplTest extends RepositoryTest {
                 .category(FeedbackCategory.ETC).member(member).crossroad(crossroad).build());
     }
 
-    @DisplayName("활동 중인 회원들의 피드백 목록 조회")
-    @Test
-    void getFeedbacks() throws Exception {
-        final Page<FeedbackResponse> allByActiveMembers = feedbackRepository.findAllByActiveMembers(
-            Pageable.ofSize(10), 0L);
-
-        assertThat(allByActiveMembers).isNotNull();
-        assertThat(allByActiveMembers.getTotalElements()).isEqualTo(3);
-        assertThat(allByActiveMembers.getTotalPages()).isEqualTo(1);
-
-        assertThat(allByActiveMembers.getContent()).isNotEmpty().allSatisfy(
-            feedback -> assertThat(feedback.getMember().getMemberStatus()).isEqualTo(
-                MemberStatus.ACTIVITY));
+    private void saveFeedback(
+        String subject, String content, FeedbackCategory category,
+        Member member, Crossroad crossroad
+    ) {
+        feedbackRepository.save(
+            Feedback.create().subject(subject).content(content).secret(Boolean.FALSE)
+                .category(category).member(member).crossroad(crossroad).build());
     }
-
-    @DisplayName("활동 중인 회원들의 날짜 조건을 추가한 피드백 목록 조회")
-    @Test
-    void getFeedbacksWithDate() throws Exception {
-        // 정렬 기준: "createdAt" 컬럼 기준 내림차순 (DESC)
-        Sort sort = Sort.by(Sort.Order.desc("createdAt"));
-
-        // 페이지 번호 0, 사이즈 10, 정렬 포함
-        Pageable pageable = PageRequest.of(0, 10, sort);
-        final Page<FeedbackResponse> page = feedbackRepository.findAll(pageable, LocalDate.MIN, LocalDate.MAX, 0L);
-
-        assertThat(page).isNotNull();
-//        assertThat(page.getTotalElements()).isEqualTo(3);
-        // TODO : 조회 결과 0으로 출력 추후 수정 필요
-        assertThat(page.getTotalElements()).isZero();
-        assertThat(page.getTotalPages()).isZero();
-
-//        assertThat(page.getContent()).isNotEmpty().allSatisfy(
-//            feedback -> assertThat(feedback.getMember().getMemberStatus()).isEqualTo(
-//                MemberStatus.ACTIVITY));
-    }
-
 }
