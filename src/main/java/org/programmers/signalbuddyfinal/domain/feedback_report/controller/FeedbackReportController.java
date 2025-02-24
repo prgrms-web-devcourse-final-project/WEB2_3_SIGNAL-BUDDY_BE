22f3830a6f36d31077be7bfb044284597c50cf65
DongminL <milkskfk12@gmail.com>
2025-02-24 15:56:32 +0900

    feat: 피드백 신고 처리(삭제) API

--- src/main/java/org/programmers/signalbuddyfinal/domain/feedback_report/controller/FeedbackReportController.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/feedback_report/controller/FeedbackReportController.java
@@ -9,21 +9,20 @@ import org.programmers.signalbuddyfinal.global.annotation.CurrentUser;
 import org.programmers.signalbuddyfinal.global.dto.CustomUser2Member;
 import org.programmers.signalbuddyfinal.global.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/feedbacks")
 @RequiredArgsConstructor
 public class FeedbackReportController {
 
     private final FeedbackReportService reportService;
 
-    @PostMapping("/{feedbackId}/reports")
+    @PostMapping("/api/feedbacks/{feedbackId}/reports")
     public ResponseEntity<ApiResponse<FeedbackReportResponse>> writeFeedbackReport(
         @PathVariable("feedbackId") long feedbackId,
         @Valid @RequestBody FeedbackReportRequest request,
@@ -40,6 +39,18 @@ public class FeedbackReportController {
     public ResponseEntity<ApiResponse<Object>> searchFeedbackReportList(
         @PathVariable("feedbackId") long feedbackId
     ) {
+        return ResponseEntity.ok(
+            ApiResponse.createSuccessWithNoData()
+        );
+    }
+
+    @DeleteMapping("/api/feedbacks/{feedbackId}/reports/{reportId}")
+    public ResponseEntity<ApiResponse<Object>> deleteFeedbackReport(
+        @PathVariable("feedbackId") long feedbackId,
+        @PathVariable("reportId") long reportId,
+        @CurrentUser CustomUser2Member user
+    ) {
+        reportService.deleteFeedbackReport(feedbackId, reportId, user);
         return ResponseEntity.ok(
             ApiResponse.createSuccessWithNoData()
         );
