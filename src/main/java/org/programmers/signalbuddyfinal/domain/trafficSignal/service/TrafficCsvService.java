35c9ee7aff1deb03d60630f96be09b2c2709bd54
Byungil Oh <dhquddlf5@gmail.com>
2025-02-24 15:46:00 +0900

    [refactor] : 파일 데이터 저장 CSV -> DB 확인하기

--- src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/service/TrafficCsvService.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/service/TrafficCsvService.java
@@ -4,20 +4,35 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+<<<<<<< HEAD
 import org.programmers.signalbuddyfinal.domain.feedback_report.exception.FeedbackReportErrorCode;
 import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberRole;
+=======
+>>>>>>> b9eea33 ([refactor] : 파일 데이터 저장)
 import org.programmers.signalbuddyfinal.domain.trafficSignal.dto.TrafficFileResponse;
 import org.programmers.signalbuddyfinal.domain.trafficSignal.entity.TrafficSignal;
 import org.programmers.signalbuddyfinal.domain.trafficSignal.exception.TrafficErrorCode;
 import org.programmers.signalbuddyfinal.domain.trafficSignal.repository.TrafficRepository;
+<<<<<<< HEAD
 import org.programmers.signalbuddyfinal.global.dto.CustomUser2Member;
+=======
+>>>>>>> b9eea33 ([refactor] : 파일 데이터 저장)
 import org.programmers.signalbuddyfinal.global.exception.BusinessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+<<<<<<< HEAD
 
 import java.io.*;
 import java.nio.charset.Charset;
+=======
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+>>>>>>> b9eea33 ([refactor] : 파일 데이터 저장)
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,10 +45,10 @@ public class TrafficCsvService {
     private final TrafficRepository trafficRepository;
 
     @Transactional
+<<<<<<< HEAD
     public void saveCsvData(File file, CustomUser2Member user) throws IOException {
 
-        // 관리자만 수정 가능
-        verifyAdmin(user);
+
 
         try (Reader reader = new BufferedReader( new InputStreamReader(new FileInputStream(file), Charset.forName("EUC-KR") ) ) ){
 
@@ -51,6 +66,20 @@ public class TrafficCsvService {
             for (TrafficFileResponse trafficFileResponse : traffics) {
                 System.out.println(trafficFileResponse.getLng());
             }
+=======
+    public void saveCsvData(MultipartFile file) throws IOException {
+
+        List<TrafficSignal> entityList = new ArrayList<>();
+        Reader reader = new BufferedReader( new InputStreamReader(file.getInputStream() ) );
+
+        try {
+            CsvToBean<TrafficFileResponse> csvToBean = new CsvToBeanBuilder<TrafficFileResponse>(reader)
+                    .withType(TrafficFileResponse.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            List<TrafficFileResponse> traffics = csvToBean.parse();
+>>>>>>> b9eea33 ([refactor] : 파일 데이터 저장)
 
             for(TrafficFileResponse trafficRes : traffics) {
                 entityList.add(new TrafficSignal(trafficRes));
@@ -59,12 +88,31 @@ public class TrafficCsvService {
             trafficRepository.saveAll(entityList);
 
         } catch (DataIntegrityViolationException e) {
+<<<<<<< HEAD
             log.error("❌ Data Integrity Violation: {}", e.getMessage(), e);
             throw new BusinessException(TrafficErrorCode.ALREADY_EXIST_TRAFFIC_SIGNAL);
+=======
+            throw new BusinessException(TrafficErrorCode.ALREADY_EXIST_TRAFFIC_SIGNAL);
+        } catch (Exception e){
+            log.error(e.getMessage());
+>>>>>>> b9eea33 ([refactor] : 파일 데이터 저장)
         }
 
     }
 
+<<<<<<< HEAD
+    // 관리자만 접근 가능
+    private void verifyAdmin(CustomUser2Member user) {
+        if (!MemberRole.ADMIN.equals(user.getRole())) {
+            throw new BusinessException(
+                    FeedbackReportErrorCode.REQUEST_NOT_AUTHORIZED
+            );
+        }
+    }
+
+=======
+>>>>>>> b9eea33 ([refactor] : 파일 데이터 저장)
+            log.error("❌ Data Integrity Violation: {}", e.getMessage(), e);
     // 관리자만 접근 가능
     private void verifyAdmin(CustomUser2Member user) {
         if (!MemberRole.ADMIN.equals(user.getRole())) {
