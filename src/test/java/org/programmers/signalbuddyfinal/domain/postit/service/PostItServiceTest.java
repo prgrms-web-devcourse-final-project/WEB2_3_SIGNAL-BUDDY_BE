444c7350c630b4c9f6bda0d00a55e497f2181617
seohyeonmen <iacsbe36@gmail.com>
2025-02-25 22:27:30 +0900

    refactor: requestDto 불필요한 변수 삭제

--- src/test/java/org/programmers/signalbuddyfinal/domain/postit/service/PostItServiceTest.java
+++ src/test/java/org/programmers/signalbuddyfinal/domain/postit/service/PostItServiceTest.java
@@ -15,6 +15,7 @@ import org.programmers.signalbuddyfinal.domain.member.entity.Member;
 import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberRole;
 import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberStatus;
 import org.programmers.signalbuddyfinal.domain.member.repository.MemberRepository;
+import org.programmers.signalbuddyfinal.domain.postit.dto.PostItCreateRequest;
 import org.programmers.signalbuddyfinal.domain.postit.dto.PostItRequest;
 import org.programmers.signalbuddyfinal.domain.postit.dto.PostItResponse;
 import org.programmers.signalbuddyfinal.domain.postit.entity.Danger;
@@ -36,11 +37,11 @@ public class PostItServiceTest extends ServiceTest {
     @Autowired
     private MemberRepository memberRepository;
 
-    private static final GeometryFactory geometryFactory = new GeometryFactory();
     private Member member1;
     private Member member2;
     private CustomUser2Member user1;
     private CustomUser2Member user2;
+    private CustomUser2Member notUser;
     MockMultipartFile mockImage1;
     MockMultipartFile mockImage2;
 
@@ -64,34 +65,36 @@ public class PostItServiceTest extends ServiceTest {
 
         user1 = new CustomUser2Member(
             new CustomUserDetails(1L, "user1@gmamil.com", "1234",
-                "url2", "user1", MemberRole.USER, MemberStatus.ACTIVITY));
+                "url2.jpg", "user1", MemberRole.USER, MemberStatus.ACTIVITY));
         user2 = new CustomUser2Member(
             new CustomUserDetails(2L, "user2@gmamil.com", "1234",
-                "url2", "user2", MemberRole.USER, MemberStatus.ACTIVITY));
+                "url2.jpg", "user2", MemberRole.USER, MemberStatus.ACTIVITY));
+        notUser = new CustomUser2Member(
+            new CustomUserDetails(10L, "notUser@gmamil.com", "1234",
+                "notUser.jpg", "notUser", MemberRole.USER, MemberStatus.ACTIVITY));
     }
 
     @Test
     @DisplayName("포스트잇 등록 성공 테스트")
-    public void createPostItTest() {
-        PostItRequest request = createPostItRequest("제목", "내용", 1L,
-            LocalDateTime.of(25, 1, 1, 0, 0, 0));
+    public void createPostItSuccessTest() {
+        PostItCreateRequest request = createPostItCreateRequest("제목", "내용",
+            LocalDateTime.of(25, 1, 1, 0, 0));
 
-        PostItResponse response = postItService.createPostIt(request, mockImage1);
+        PostItResponse response = postItService.createPostIt(request, mockImage1, user1);
 
         assertThat(response.getContent()).isEqualTo(request.getContent());
         assertThat(response.getSubject()).isEqualTo(request.getSubject());
-        assertThat(response.getMemberId()).isEqualTo(request.getMemberId());
 
     }
 
     @Test
     @DisplayName("비회원 포스트잇 등록시 예외 발생 테스트")
     public void nonMemberCreatePostItTest() {
-        PostItRequest request = createPostItRequest("제목", "내용", 3L,
-            LocalDateTime.of(25, 1, 1, 0, 0, 0));
+        PostItCreateRequest request = createPostItCreateRequest("제목", "내용",
+            LocalDateTime.of(25, 1, 1, 0, 0));
 
         assertThrows(BusinessException.class,
-            () -> postItService.createPostIt(request, mockImage1));
+            () -> postItService.createPostIt(request, mockImage1, notUser));
     }
 
     @Test
@@ -100,9 +103,8 @@ public class PostItServiceTest extends ServiceTest {
 
         postItRepository.save(
             createPostIt(Danger.NOTICE, PointUtil.toPoint(1.0203, 1.3048), "제목1",
-                "제목1", "img1", LocalDateTime.of(2025, 1, 1, 0, 0, 0), member1));
-        PostItRequest request = createPostItRequest("제목", "내용", 1L,
-            LocalDateTime.of(25, 1, 1, 0, 0, 0));
+                "제목1", "img1", LocalDateTime.of(2025, 1, 1, 0, 0), member1));
+        PostItRequest request = createPostItRequest("제목", "내용");
 
         PostItResponse response = postItService.updatePostIt(1L, request, mockImage2, user1);
 
@@ -116,20 +118,19 @@ public class PostItServiceTest extends ServiceTest {
 
         postItRepository.save(
             createPostIt(Danger.NOTICE, PointUtil.toPoint(1.0203, 1.3048), "제목1",
-                "제목1", "img1", LocalDateTime.of(2025, 1, 1, 0, 0, 0), member1));
-        PostItRequest request = createPostItRequest("제목", "내용", 1L,
-            LocalDateTime.of(25, 1, 1, 0, 0, 0));
+                "제목1", "img1", LocalDateTime.of(2025, 1, 1, 0, 0), member1));
+        PostItRequest request = createPostItRequest("제목", "내용");
 
         assertThrows(BusinessException.class,
-            () -> postItService.updatePostIt(1L, request, mockImage2, user2));
+            () -> postItService.updatePostIt(1L, request, mockImage2, notUser));
     }
 
     @Test
     @DisplayName("포스트잇 삭제 성공 테스트")
-    public void deletePostItSuccessTest(){
+    public void deletePostItSuccessTest() {
         postItRepository.save(
             createPostIt(Danger.NOTICE, PointUtil.toPoint(1.0203, 1.3048), "제목1",
-                "제목1", "img1", LocalDateTime.of(2025, 1, 1, 0, 0, 0), member1));
+                "제목1", "img1", LocalDateTime.of(2025, 1, 1, 0, 0), member1));
 
         postItService.deletePostIt(1L, user1);
 
@@ -142,26 +143,35 @@ public class PostItServiceTest extends ServiceTest {
 
         postItRepository.save(
             createPostIt(Danger.NOTICE, PointUtil.toPoint(1.0203, 1.3048), "제목1",
-                "제목1", "img1", LocalDateTime.of(2025, 1, 1, 0, 0, 0), member1));
-        PostItRequest request = createPostItRequest("제목", "내용", 1L,
-            LocalDateTime.of(25, 1, 1, 0, 0, 0));
+                "제목1", "img1", LocalDateTime.of(2025, 1, 1, 0, 0), member1));
+        PostItRequest request = createPostItRequest("제목", "내용");
 
         assertThrows(BusinessException.class,
             () -> postItService.deletePostIt(1L, user2));
     }
 
-    private PostItRequest createPostItRequest(String subject, String content, Long memberId,
-        LocalDateTime createDate) {
+    private PostItRequest createPostItRequest(String subject, String content) {
 
         return PostItRequest.builder()
             .danger(Danger.NOTICE)
             .lat(1.02222)
             .lng(1.09099)
+            .imageUrl("https://image.com/imageUrl")
             .subject(subject)
             .content(content)
-            .imageUrl("imageUrl")
+            .build();
+    }
+
+    private PostItCreateRequest createPostItCreateRequest(String subject, String content,
+        LocalDateTime createDate) {
+
+        return PostItCreateRequest.builder()
+            .danger(Danger.NOTICE)
+            .lat(1.02222)
+            .lng(1.09099)
+            .subject(subject)
+            .content(content)
             .createDate(createDate)
-            .memberId(memberId)
             .build();
     }
 
