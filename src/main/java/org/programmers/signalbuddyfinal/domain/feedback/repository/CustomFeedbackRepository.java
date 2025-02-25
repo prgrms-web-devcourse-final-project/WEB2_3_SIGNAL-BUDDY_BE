c812ac0e992970ded6f4d38ffd6fc5cc9711766f
DongminL <milkskfk12@gmail.com>
2025-02-25 21:33:12 +0900

    feat: 관리자용 피드백 조회 쿼리 추가

--- src/main/java/org/programmers/signalbuddyfinal/domain/feedback/repository/CustomFeedbackRepository.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/feedback/repository/CustomFeedbackRepository.java
@@ -3,6 +3,7 @@ package org.programmers.signalbuddyfinal.domain.feedback.repository;
 import java.time.LocalDate;
 import java.util.Set;
 import org.programmers.signalbuddyfinal.domain.feedback.dto.FeedbackResponse;
+import org.programmers.signalbuddyfinal.domain.feedback.entity.Feedback;
 import org.programmers.signalbuddyfinal.domain.feedback.entity.enums.AnswerStatus;
 import org.programmers.signalbuddyfinal.domain.feedback.entity.enums.FeedbackCategory;
 import org.springframework.data.domain.Page;
@@ -18,9 +19,13 @@ public interface CustomFeedbackRepository {
 
     Page<FeedbackResponse> findPagedExcludingMember(Long memberId, Pageable pageable);
 
-    Page<FeedbackResponse> findAll(
-        Pageable pageable,
+    Page<FeedbackResponse> findAllByFilter(
+        Pageable pageable, String keyword,
+        AnswerStatus answerStatus,
+        Set<FeedbackCategory> categories,
         LocalDate startDate, LocalDate endDate,
-        AnswerStatus answerStatus
+        Boolean deleted
     );
+
+    Feedback findByIdOrThrow(Long id);
 }
