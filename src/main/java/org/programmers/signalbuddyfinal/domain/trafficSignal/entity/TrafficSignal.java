dbddab8a66208b76a98b407d3757eea05566f128
Byungil Oh <dhquddlf5@gmail.com>
2025-02-27 16:10:00 +0900

    rename : 디렉토리명 변경으로 변경 trafficSignal

--- src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/entity/TrafficSignal.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/entity/TrafficSignal.java
@@ -1,4 +1,4 @@
-package org.programmers.signalbuddyfinal.domain.trafficsignal.entity;
+package org.programmers.signalbuddyfinal.domain.trafficSignal.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
