b5c68a041271f76088ff6388f5d83f83f721459e
seohyeonmen <iacsbe36@gmail.com>
2025-02-27 15:31:14 +0900

    test: 관리자 포스트잇 해결 테스트

--- src/test/java/org/programmers/signalbuddyfinal/domain/admin/controller/AdminPostItControllerTest.java
+++ src/test/java/org/programmers/signalbuddyfinal/domain/admin/controller/AdminPostItControllerTest.java
@@ -3,32 +3,47 @@ package org.programmers.signalbuddyfinal.domain.admin.controller;
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.programmers.signalbuddyfinal.global.support.RestDocsFormatGenerators.commonResponseFormat;
+import static org.programmers.signalbuddyfinal.global.support.RestDocsFormatGenerators.getTokenExample;
 import static org.programmers.signalbuddyfinal.global.support.RestDocsFormatGenerators.pageResponseFormat;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.epages.restdocs.apispec.ResourceDocumentation;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.SimpleType;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.programmers.signalbuddyfinal.domain.admin.dto.AdminPostItResponse;
 import org.programmers.signalbuddyfinal.domain.admin.service.AdminPostItService;
+import org.programmers.signalbuddyfinal.domain.postit.dto.PostItResponse;
 import org.programmers.signalbuddyfinal.domain.postit.entity.Danger;
+import org.programmers.signalbuddyfinal.global.anotation.WithMockCustomUser;
 import org.programmers.signalbuddyfinal.global.dto.PageResponse;
 import org.programmers.signalbuddyfinal.global.support.ControllerTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.ResultActions;
 import org.testcontainers.shaded.org.apache.commons.lang3.ArrayUtils;
 
 @WebMvcTest(AdminPostItController.class)
@@ -90,6 +105,96 @@ public class AdminPostItControllerTest extends ControllerTest {
                             )
                         ).build()
                 )));
+    }
+
+    @Test
+    @DisplayName("관리자 포스트잇 해결")
+    @WithMockCustomUser
+    public void completePostIt() throws Exception {
+        PostItResponse postItResponse = createResponse(1L);
+
+        given(adminPostItService.completePostIt(anyLong())).willReturn(postItResponse);
+
+        final ResultActions result = mockMvc.perform(patch("/api/admin/postits/{postitId}",1L)
+            .with(csrf())
+            .header(HttpHeaders.AUTHORIZATION, getTokenExample())
+        );
+
+        result.andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andDo(
+                document(
+                    "관리자 포스트잇 해결",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    resource(
+                        ResourceSnippetParameters.builder()
+                            .tag(tag)
+                            .summary("포스트잇을 해결상태로 변경하는 API")
+                            .pathParameters(
+                                ResourceDocumentation.parameterWithName("postitId").type(SimpleType.NUMBER)
+                                    .description("해결하려는 포스트잇 ID")
+                            )
+                            .build()
+                    ),
+                    responseFields(
+                        ArrayUtils.addAll(
+                            commonResponseFormat(),
+                            fieldWithPath("data.postitId")
+                                .type(JsonFieldType.NUMBER)
+                                .description("생성된 포스트잇 ID"),
+                            fieldWithPath("data.danger")
+                                .type(JsonFieldType.STRING)
+                                .description("포스트잇 위험도"),
+                            fieldWithPath("data.lat")
+                                .type(JsonFieldType.NUMBER)
+                                .description("등록 위도"),
+                            fieldWithPath("data.lng")
+                                .type(JsonFieldType.NUMBER)
+                                .description("등록 경도"),
+                            fieldWithPath("data.subject")
+                                .type(JsonFieldType.STRING)
+                                .description("포스트잇 제목"),
+                            fieldWithPath("data.content")
+                                .type(JsonFieldType.STRING)
+                                .description("포스트잇 내용"),
+                            fieldWithPath("data.imageUrl")
+                                .type(JsonFieldType.STRING)
+                                .description("등록된 이미지 URL"),
+                            fieldWithPath("data.expiryDate")
+                                .type(JsonFieldType.STRING)
+                                .description("""
+                                    포스트잇 삭제 예정일
+                                    형식 : YYYY-MM-dd HH-mm
+                                    """),
+                            fieldWithPath("data.createDate")
+                                .type(JsonFieldType.STRING)
+                                .description("""
+                                    포스트잇 등록일
+                                    형식 : YYYY-MM-dd HH-mm
+                                    """),
+                            fieldWithPath("data.memberId")
+                                .type(JsonFieldType.NUMBER)
+                                .description("등록한 사용자 ID")
+                        )
+                    )
+                )
+            );
+    }
+
+    private PostItResponse createResponse(Long memberId) {
+        return PostItResponse.builder()
+            .postitId(1L)
+            .danger(Danger.WARNING) // 수정된 오타
+            .lat(37.5206868)
+            .lng(127.1171114)
+            .subject("포스트잇 제목")
+            .content("포스트잇 내용")
+            .imageUrl("https://image.com/imageUrl")
+            .expiryDate(LocalDateTime.of(2025, 1, 8, 1, 25))
+            .createDate(LocalDateTime.of(2025, 1, 1, 1, 25))
+            .memberId(memberId)
+            .build();
     }
 
 }
--- src/test/java/org/programmers/signalbuddyfinal/domain/admin/service/AdminPostItServiceTest.java
+++ src/test/java/org/programmers/signalbuddyfinal/domain/admin/service/AdminPostItServiceTest.java
@@ -1,7 +1,9 @@
 package org.programmers.signalbuddyfinal.domain.admin.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import groovy.util.logging.Slf4j;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,15 +19,22 @@ import org.programmers.signalbuddyfinal.domain.member.repository.MemberRepositor
 import org.programmers.signalbuddyfinal.domain.postit.entity.Danger;
 import org.programmers.signalbuddyfinal.domain.postit.entity.Postit;
 import org.programmers.signalbuddyfinal.domain.postit.repository.PostItRepository;
+import org.programmers.signalbuddyfinal.domain.postitsolve.repository.PostitSolveRepository;
+import org.programmers.signalbuddyfinal.global.exception.BusinessException;
 import org.programmers.signalbuddyfinal.global.support.ServiceTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+@Slf4j
 public class AdminPostItServiceTest extends ServiceTest {
 
+    private static final Logger log = LoggerFactory.getLogger(AdminPostItServiceTest.class);
     List<Member> member;
     Pageable pageable;
+    Postit unsolvedPostit;
 
     @Autowired
     PostItRepository postItRepository;
@@ -33,6 +42,8 @@ public class AdminPostItServiceTest extends ServiceTest {
     MemberRepository memberRepository;
     @Autowired
     AdminPostItService adminPostItService;
+    @Autowired
+    PostitSolveRepository postitSolveRepository;
 
     @BeforeEach
     public void setUp() {
@@ -45,6 +56,12 @@ public class AdminPostItServiceTest extends ServiceTest {
                 LocalDateTime.of(2025, 1, 2, 1, 30), LocalDateTime.of(2025, 1, 8, 1, 30),
                 member.get(i));
         }
+
+        unsolvedPostit = createPostIt(Danger.WARNING, PointUtil.toPoint(1.0203, 1.3048), "제목1",
+            "내용1",
+            "https://image1.com/imageUrl",
+            LocalDateTime.of(2025, 1, 2, 1, 30), null,
+            member.get(0));
         pageable = PageRequest.of(0, 10);
     }
 
@@ -52,8 +69,25 @@ public class AdminPostItServiceTest extends ServiceTest {
     @Test
     public void getAllPostItSuccessTest() {
 
-        assertThat(adminPostItService.getAllPostIt(pageable).getTotalElements()).isEqualTo(20);
+        assertThat(adminPostItService.getAllPostIt(pageable).getTotalElements()).isEqualTo(21);
         assertThat(adminPostItService.getAllPostIt(pageable).getPageSize()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("관리자 포스트잇 해결 상태 변경 성공 테스트")
+    public void adminCompletePostItSuccessTest() {
+        Long postitId = unsolvedPostit.getPostitId();
+        adminPostItService.completePostIt(postitId);
+
+        log.info("결과..." + unsolvedPostit.getDeletedAt().toString());
+        assertThat(unsolvedPostit.getDeletedAt()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("관리자 이미 해결된 포스트잇 변경 예외 발생 테스트")
+    public void adminAlreadyCompletePostItExceptionTest() {
+
+        assertThrows(BusinessException.class, () -> adminPostItService.completePostIt(1L));
     }
 
     private Postit createPostIt(Danger danger, Point coordinate, String subject,
