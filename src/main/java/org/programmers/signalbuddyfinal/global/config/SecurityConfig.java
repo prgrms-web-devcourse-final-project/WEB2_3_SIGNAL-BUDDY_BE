e90dd68bc835f59cd0f984ed7814333ef71dea77
DongminL <milkskfk12@gmail.com>
2025-02-27 18:24:39 +0900

    chore: Feedback Summary API는 관리자만 사용 가능하도록 설정

--- src/main/java/org/programmers/signalbuddyfinal/global/config/SecurityConfig.java
+++ src/main/java/org/programmers/signalbuddyfinal/global/config/SecurityConfig.java
@@ -89,10 +89,16 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/admin/feedbacks").hasRole(ADMIN)
                 // 피드백 신고
                 .requestMatchers(HttpMethod.GET, "/api/feedbacks/reports").hasRole(ADMIN)
-                .requestMatchers(HttpMethod.PATCH, "/api/feedbacks/{feedbackId}/reports/{reportId}")
-                .hasRole(ADMIN)
-                .requestMatchers(HttpMethod.DELETE,
-                    "/api/feedbacks/{feedbackId}/reports/{reportId}").hasRole(ADMIN)
+                .requestMatchers(
+                    HttpMethod.PATCH,
+                    "/api/feedbacks/{feedbackId}/reports/{reportId}"
+                ).hasRole(ADMIN)
+                .requestMatchers(
+                    HttpMethod.DELETE,
+                    "/api/feedbacks/{feedbackId}/reports/{reportId}"
+                ).hasRole(ADMIN)
+                // 피드백 통계
+                .requestMatchers("/api/feedback-summary/**").hasRole(ADMIN)
                 // 회원
                 .requestMatchers("/api/admins/**", "/admins/members/**").hasRole(ADMIN)
                 .requestMatchers("/api/members/**", "/members/**").hasRole(USER)
