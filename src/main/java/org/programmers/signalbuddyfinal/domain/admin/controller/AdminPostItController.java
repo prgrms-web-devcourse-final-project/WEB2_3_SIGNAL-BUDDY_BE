4ea02533bfbe3183fdf968f184476414e075cba9
seohyeonmen <iacsbe36@gmail.com>
2025-02-28 11:45:35 +0900

    feat: 포스트잇 관리 필터링 조회 구현

--- src/main/java/org/programmers/signalbuddyfinal/domain/admin/controller/AdminPostItController.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/admin/controller/AdminPostItController.java
@@ -1,17 +1,17 @@
 package org.programmers.signalbuddyfinal.domain.admin.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.programmers.signalbuddyfinal.domain.admin.dto.AdminMemberResponse;
 import org.programmers.signalbuddyfinal.domain.admin.dto.AdminPostItResponse;
+import org.programmers.signalbuddyfinal.domain.admin.dto.PostItFilterRequest;
 import org.programmers.signalbuddyfinal.domain.admin.service.AdminPostItService;
 import org.programmers.signalbuddyfinal.domain.postit.dto.PostItResponse;
-import org.programmers.signalbuddyfinal.domain.postit.service.PostItService;
 import org.programmers.signalbuddyfinal.global.dto.PageResponse;
 import org.programmers.signalbuddyfinal.global.response.ApiResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +39,16 @@ public class AdminPostItController {
         return ResponseEntity.ok(
             ApiResponse.createSuccess(
                adminPostItService.completePostIt(postitId)));
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<ApiResponse<PageResponse<AdminPostItResponse>>> getAllFilteredPostIt(
+        @PageableDefault(page = 0, size = 10) Pageable pageable,
+        @ModelAttribute PostItFilterRequest postItFilterRequest) {
+
+        PageResponse<AdminPostItResponse> postIts = adminPostItService.getAllPostItWithFilter(pageable,
+            postItFilterRequest);
+
+        return ResponseEntity.ok(ApiResponse.createSuccess(postIts));
     }
 }
--- src/main/java/org/programmers/signalbuddyfinal/domain/postit/repository/CustomPostItRepositoryImpl.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/postit/repository/CustomPostItRepositoryImpl.java
@@ -4,10 +4,18 @@ import static org.programmers.signalbuddyfinal.domain.postit.entity.QPostit.post
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.QBean;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.programmers.signalbuddyfinal.domain.admin.dto.AdminPostItResponse;
+import org.programmers.signalbuddyfinal.domain.admin.dto.PostItFilterRequest;
+import org.programmers.signalbuddyfinal.domain.admin.dto.enums.Deleted;
+import org.programmers.signalbuddyfinal.domain.admin.dto.enums.Periods;
+import org.programmers.signalbuddyfinal.domain.postit.entity.Danger;
 import org.programmers.signalbuddyfinal.global.dto.PageResponse;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -41,5 +49,96 @@ public class CustomPostItRepositoryImpl implements CustomPostItRepository {
             .fetchCount();
 
         return new PageResponse<>(new PageImpl<>(postIts, pageable, total));
+    }
+
+    @Override
+    public PageResponse<AdminPostItResponse> findAllPostItWithFilter(Pageable pageable,
+        PostItFilterRequest filter) {
+
+        List<AdminPostItResponse> postIts = jpaQueryFactory
+            .select(adminPostItResponseDto)
+            .from(postit)
+            .where(
+                eqSearch(filter.getSearch())
+                    .and(eqDanger(filter.getDanger()))
+                    .and(eqIsDeleted(filter.getDeleted()))
+                    .and(betweenCreatedAt(filter.getStartDate(), filter.getEndDate()))
+                    .and(withinSignupPeriod(filter.getPeriods()))
+            )
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .orderBy(postit.createdAt.desc())
+            .fetch();
+
+        long total = jpaQueryFactory
+            .select(adminPostItResponseDto)
+            .from(postit)
+            .where(
+                eqSearch(filter.getSearch()),
+                eqDanger(filter.getDanger()),
+                eqIsDeleted(filter.getDeleted()),
+                betweenCreatedAt(filter.getStartDate(), filter.getEndDate()),
+                withinSignupPeriod(filter.getPeriods())
+            )
+            .fetchCount();
+
+        return new PageResponse<>(new PageImpl<>(postIts, pageable, total));
+    }
+
+    private BooleanExpression eqDanger(Danger danger) {
+        return (danger != null ? postit.danger.eq(danger) : Expressions.TRUE);
+    }
+
+    // 해결 조회
+    private BooleanExpression eqIsDeleted(Deleted deleted) {
+        if (deleted == null) {
+            return null;
+        }
+        return deleted == Deleted.DELETED ? postit.deletedAt.isNotNull() : postit.deletedAt.isNull();
+    }
+
+    // 검색
+    private BooleanExpression eqSearch(String search) {
+        return ((search != null && !search.isEmpty()) ? postit.content.contains(search)
+            .or(postit.subject.contains(search)) : Expressions.TRUE);
+    }
+
+    // 기간 조회
+    private BooleanExpression betweenCreatedAt(LocalDateTime startDate, LocalDateTime endDate) {
+        if (startDate != null && endDate != null) {
+            return postit.expiryDate.between(startDate, endDate);
+        }
+        return Expressions.TRUE;
+    }
+
+    // 이전 날짜 조회
+    private BooleanExpression withinSignupPeriod(Periods period) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime startDate;
+
+        if (period == null) {
+            return Expressions.TRUE;
+        }
+
+        switch (period) {
+            case TODAY:
+                startDate = now.truncatedTo(ChronoUnit.DAYS);
+                break;
+            case THREE_DAYS:
+                startDate = now.minusDays(3);
+                break;
+            case WEEK:
+                startDate = now.minusWeeks(1);
+                break;
+            case MONTH:
+                startDate = now.minusMonths(1);
+                break;
+            case THREE_MONTH:
+                startDate = now.minusMonths(3);
+                break;
+            default:
+                return Expressions.TRUE;
+        }
+        return postit.expiryDate.between(startDate, now);
     }
 }
