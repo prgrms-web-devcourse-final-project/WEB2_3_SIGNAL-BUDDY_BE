4b979af2e236726370fbbba6823b499a6b50ac2a
seohyeonmen <iacsbe36@gmail.com>
2025-02-28 14:25:52 +0900

    doc: 포스트잇 관리 api 명세서 작성

--- src/test/java/org/programmers/signalbuddyfinal/domain/admin/controller/AdminPostItControllerTest.java
+++ src/test/java/org/programmers/signalbuddyfinal/domain/admin/controller/AdminPostItControllerTest.java
@@ -28,8 +28,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.programmers.signalbuddyfinal.domain.admin.dto.AdminMemberResponse;
 import org.programmers.signalbuddyfinal.domain.admin.dto.AdminPostItResponse;
+import org.programmers.signalbuddyfinal.domain.admin.dto.MemberFilterRequest;
+import org.programmers.signalbuddyfinal.domain.admin.dto.PostItFilterRequest;
+import org.programmers.signalbuddyfinal.domain.admin.dto.enums.Deleted;
 import org.programmers.signalbuddyfinal.domain.admin.service.AdminPostItService;
+import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberRole;
+import org.programmers.signalbuddyfinal.domain.member.entity.enums.MemberStatus;
 import org.programmers.signalbuddyfinal.domain.postit.dto.PostItResponse;
 import org.programmers.signalbuddyfinal.domain.postit.entity.Danger;
 import org.programmers.signalbuddyfinal.global.anotation.WithMockCustomUser;
@@ -145,7 +151,12 @@ public class AdminPostItControllerTest extends ControllerTest {
                                 .description("생성된 포스트잇 ID"),
                             fieldWithPath("data.danger")
                                 .type(JsonFieldType.STRING)
-                                .description("포스트잇 위험도"),
+                                .description("""
+                                    포스트잇 위험도
+                                    - DANGER : 위험
+                                    - WARNING : 경고
+                                    - NOTICE : 안내
+                                    """),
                             fieldWithPath("data.lat")
                                 .type(JsonFieldType.NUMBER)
                                 .description("등록 위도"),
@@ -180,6 +191,120 @@ public class AdminPostItControllerTest extends ControllerTest {
                     )
                 )
             );
+    }
+    @DisplayName("포스트잇 필터링 조회")
+    @Test
+    public void FilteredPostIt() throws Exception {
+        final PostItFilterRequest filter = PostItFilterRequest.builder()
+            .startDate(LocalDateTime.of(2025, 1, 1, 0, 0, 0))
+            .endDate(LocalDateTime.of(2025, 2, 20, 0, 0, 0))
+            .search("검색어")
+            .periods(null)
+            .danger(Danger.NOTICE)
+            .deleted(Deleted.NOTDELETED)
+            .build();
+
+        final List<AdminPostItResponse> postIts = List.of(
+            new AdminPostItResponse(Danger.NOTICE, "제목1", "내용1", "user1@test.com",
+                LocalDateTime.of(2025, 1, 20, 11, 35)),
+            new AdminPostItResponse(Danger.NOTICE, "제목1", "내용1", "user1@test.com",
+                LocalDateTime.of(2025, 1, 20, 11, 35))
+        );
+
+        final Pageable pageable = PageRequest.of(0, 10);
+        final PageResponse<AdminPostItResponse> response = new PageResponse<>(
+            new PageImpl<>(postIts, pageable,
+                postIts.size()));
+
+        given(adminPostItService.getAllPostItWithFilter(any(Pageable.class),
+            any(PostItFilterRequest.class)))
+            .willReturn(response);
+
+        final ResultActions result = mockMvc.perform(
+                get("/api/admin/postits/filter")
+                    .param("page", "0")
+                    .param("size", "10")
+                    .param("startDate", filter.getStartDate().toString())
+                    .param("endDate", filter.getEndDate().toString())
+                    .param("period",  filter.getPeriods() != null ? filter.getPeriods().toString() : null)
+                    .param("search", filter.getSearch())
+                    .param("danger", filter.getSearch().toString())
+                    .param("deleted", filter.getSearch().toString())
+                    .header("Accept", "application/json"))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andDo(document("관리자 포스트잇 필터링 조회",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                resource(
+                    ResourceSnippetParameters.builder()
+                        .tag(tag)
+                        .summary("관리자 포스트잇 필터링 조회")
+                        .description("관리자가 포스트잇을 필터링해 조회하는 API")
+                        .queryParameters(
+                            parameterWithName("page").description("페이지 번호"),
+                            parameterWithName("size").description("페이지 크기"),
+                            parameterWithName("startDate").description("""
+                                    (선택) 가입 기간별 조회 1
+                                          * 조회 기간 : startDate ~ endDate
+                                          * 사용시 startDate endDate 둘 다 입력 필수
+                                    - 검색 시작일
+                                    - 형식 : YYYY-MM-dd
+                                    """).optional(),
+                            parameterWithName("endDate").description("""
+                                    - 검색 종료일
+                                    - 형식 : YYYY-MM-dd
+                                    """)
+                                .optional(),
+                            parameterWithName("period").description(
+                                    """
+                                (선택) 가입 기간별 조회 2
+                                      * 조회 기간 : 현재일 기준
+                                - 형식 : YYYY-MM-dd
+                                -- TODAY : 오늘
+                                -- THREE_DAYS : 3일 전
+                                -- WEEK : 일주일 전
+                                -- MONTH : 한달 전
+                                -- THREE_MONTH : 세달 전
+                                
+                                가입 기간별 조회1, 2 중복 사용 불가
+                                """),
+                            parameterWithName("danger").description("""
+                                    (선택) 포스트잇 위험도
+                                    - DANGER : 위험
+                                    - WARNING : 경고
+                                    - NOTICE : 안내
+                                    """).optional(),
+                            parameterWithName("deleted").description("""
+                                (선택) 해결/만료 여부
+                                형식
+                                - DELETED : 해결
+                                - NOTDELETED : 미해결
+                                """)
+                                .optional(),
+                            parameterWithName("search").description(
+                                    """
+                                검색
+                                - 제목, 내용
+                                """)
+                                .optional()
+                        )
+                        .responseFields(
+                            ArrayUtils.addAll(
+                                pageResponseFormat(),
+                                fieldWithPath("data.searchResults[].danger").description("""
+                                    위험도
+                                    - DANGER : 위험
+                                    - WARNING : 경고
+                                    - NOTICE : 안내
+                                    """),
+                                fieldWithPath("data.searchResults[].subject").description("제목"),
+                                fieldWithPath("data.searchResults[].content").description("내용"),
+                                fieldWithPath("data.searchResults[].email").description("작성자"),
+                                fieldWithPath("data.searchResults[].expireDate").description("만료일")
+                            )
+                        ).build()
+                )));
     }
 
     private PostItResponse createResponse(Long memberId) {
