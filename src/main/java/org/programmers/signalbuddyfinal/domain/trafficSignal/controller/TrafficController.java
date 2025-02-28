cf6eca0f409fefdf7971ae93a7cc0efccde607ed
Byungil Oh <dhquddlf5@gmail.com>
2025-02-28 19:43:50 +0900

    refactor : 충돌 해결

--- src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/controller/TrafficController.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/controller/TrafficController.java
@@ -2,6 +2,8 @@ package org.programmers.signalbuddyfinal.domain.trafficSignal.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.programmers.signalbuddyfinal.domain.trafficSignal.service.TrafficCsvService;
+import org.programmers.signalbuddyfinal.global.annotation.CurrentUser;
+import org.programmers.signalbuddyfinal.global.dto.CustomUser2Member;
 import org.programmers.signalbuddyfinal.global.response.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,7 +25,10 @@ public class TrafficController {
     private TrafficCsvService trafficCsvService;
 
     @PostMapping("/save")
-    public ResponseEntity<ApiResponse<Object>> saveTrafficData(@RequestBody Map<String,String> request) throws IOException {
+    public ResponseEntity<ApiResponse<Object>> saveTrafficData(
+            @RequestBody Map<String,String> request,
+            @CurrentUser CustomUser2Member user
+    ) throws IOException {
         String filePath = request.get("filePath");
         File file = new File(filePath);
 
@@ -31,7 +36,7 @@ public class TrafficController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.createError("파일을 찾을 수 없습니다."));
         }
 
-        trafficCsvService.saveCsvData(file);
+        trafficCsvService.saveCsvData(file, user);
 
         return ResponseEntity.ok(ApiResponse.createSuccess("파일이 성공적으로 저장되었습니다."));
     }
