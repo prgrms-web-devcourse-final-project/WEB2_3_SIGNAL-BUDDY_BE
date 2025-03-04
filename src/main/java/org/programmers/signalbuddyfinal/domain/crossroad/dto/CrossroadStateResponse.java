3c94cd9f412ed0b61d6d2a915c68d695f01c0dbe
Lee Dongmin <milkskfk12@gmail.com>
2025-03-04 21:03:04 +0900

    [Feature] 메인   신호등 상태 정보 캐싱 (#120)
    * refactor: 코드 스타일 변경
    
    * fix: 던지는 예외 변경
    
    * feat: 전송 시간 데이터 추가로 받아오기
    
    * feat: 클라이언트에 반환하는 response dto 생성
    
    * feat: 신호등 잔여시간 데이터 캐싱하기
    
    * feat: 신호등 잔여시간 API
    
    * docs: 신호등 잔여시간 API 문서화
    
    * chore: 테스트를 위한 외부 api 실제 응답값 파일로 저장
    
    * test: 외부 api 요청 테스트
    
    * test: 신호등의 잔여 시간 반환 및 캐싱 테스트

---
+++ src/main/java/org/programmers/signalbuddyfinal/domain/crossroad/dto/CrossroadStateResponse.java
@@ -0,0 +1,65 @@
+package org.programmers.signalbuddyfinal.domain.crossroad.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.stream.Stream;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@EqualsAndHashCode
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CrossroadStateResponse implements Serializable {
+
+    private Long crossroadId;
+
+    private Long transTimestamp;
+
+    private Integer northTimeLeft;
+
+    private Integer eastTimeLeft;
+
+    private Integer southTimeLeft;
+
+    private Integer westTimeLeft;
+
+    private Integer northeastTimeLeft;
+
+    private Integer northwestTimeLeft;
+
+    private Integer southwestTimeLeft;
+
+    private Integer southeastTimeLeft;
+
+    private SignalState northState;
+
+    private SignalState eastState;
+
+    private SignalState westState;
+
+    private SignalState southState;
+
+    private SignalState northeastState;
+
+    private SignalState northwestState;
+
+    private SignalState southeastState;
+
+    private SignalState southwestState;
+
+    public int minTimeLeft() {
+        return Stream.of(
+            northTimeLeft, eastTimeLeft, southTimeLeft, westTimeLeft,
+                northeastTimeLeft, northwestTimeLeft, southwestTimeLeft, southeastTimeLeft
+            )
+            .filter(Objects::nonNull)  // null 값 제거
+            .min(Integer::compare)  // 최소값 구하기
+            .orElse(0); // 모든 값이 null일 경우
+    }
+}
