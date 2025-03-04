9e90fc4549b6f90a3652f0f59bf063a26c9438b6
DongminL <milkskfk12@gmail.com>
2025-03-04 17:10:29 +0900

    fix: 던지는 예외 변경

--- src/main/java/org/programmers/signalbuddyfinal/domain/crossroad/dto/SignalState.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/crossroad/dto/SignalState.java
@@ -1,32 +1,29 @@
 package org.programmers.signalbuddyfinal.domain.crossroad.dto;
 
-
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.programmers.signalbuddyfinal.global.exception.BusinessException;
+import org.programmers.signalbuddyfinal.global.exception.GlobalErrorCode;
 
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SignalState {
     GREEN("protected-Movement-Allowed",true),   // 신호등이 녹색, 이동 보장 상태
-    GRAY("permissive-Movement-Allowed",true), // 신호등이 황색, 이동 가능 상태
+    YELLOW("permissive-Movement-Allowed",true), // 신호등이 황색, 이동 가능 상태
     RED("stop-And-Remain",false);               // 신호등이 적색, 정지 상태
 
-    private String state;
-    private boolean can_cross;
-
-    @JsonValue // 데이터 직렬화 java -> JSON
-    public String getState() {
-        return name().toLowerCase();
-    }
+    private final String state;
+    private final boolean canCross;
 
     @JsonCreator // 데이터 역직렬화 JSON -> java
     public static SignalState fromState(String state) {
         for(SignalState signalState : SignalState.values()) {
-            if(signalState.state.equals(state)) {
+            if(signalState.state.equals(state) || signalState.name().equals(state)) {
                 return signalState;
             }
         }
-        throw new IllegalArgumentException("Unknown name: " + state);
+        throw new BusinessException(GlobalErrorCode.BAD_REQUEST);
     }
 }
