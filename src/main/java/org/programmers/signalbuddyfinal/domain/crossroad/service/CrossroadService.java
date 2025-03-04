d7bcb0a8057c73a0c2b86f43370039239a57c14a
DongminL <milkskfk12@gmail.com>
2025-03-04 17:14:05 +0900

    feat: 신호등 잔여시간 데이터 캐싱하기

--- src/main/java/org/programmers/signalbuddyfinal/domain/crossroad/service/CrossroadService.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/crossroad/service/CrossroadService.java
@@ -1,20 +1,24 @@
 package org.programmers.signalbuddyfinal.domain.crossroad.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.programmers.signalbuddyfinal.domain.crossroad.dto.CrossroadApiResponse;
 import org.programmers.signalbuddyfinal.domain.crossroad.dto.CrossroadStateApiResponse;
+import org.programmers.signalbuddyfinal.domain.crossroad.dto.CrossroadStateResponse;
 import org.programmers.signalbuddyfinal.domain.crossroad.entity.Crossroad;
 import org.programmers.signalbuddyfinal.domain.crossroad.exception.CrossroadErrorCode;
+import org.programmers.signalbuddyfinal.domain.crossroad.mapper.CrossroadMapper;
 import org.programmers.signalbuddyfinal.domain.crossroad.repository.CrossroadRepository;
 import org.programmers.signalbuddyfinal.global.exception.BusinessException;
 import org.programmers.signalbuddyfinal.global.monitoring.HttpRequestManager;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @Service
@@ -25,6 +29,9 @@ public class CrossroadService {
     private final CrossroadRepository crossroadRepository;
     private final CrossroadProvider crossroadProvider;
     private final HttpRequestManager httpRequestManager;
+    private final RedisTemplate<Object, Object> redisTemplate;
+
+    private static final String STATE_PREFIX = "crossroad-state:";
 
     @Transactional
     public void saveCrossroadDates(int page, int pageSize) {
@@ -45,10 +52,29 @@ public class CrossroadService {
         }
     }
 
-    public List<CrossroadStateApiResponse> checkSignalState(Long id) { // id값으로 신호등의 상태를 검색
+    public CrossroadStateResponse checkSignalState(Long crossroadId) {
+        httpRequestManager.increase(crossroadId);
 
-        httpRequestManager.increase(id);
-        return crossroadProvider.requestCrossroadStateApi(id);
+        CrossroadStateResponse cache = getStateCache(crossroadId);
+        if (cache != null && cache.getTransTimestamp() != null) {
+            return cache;
+        }
+
+        Crossroad crossroad = crossroadRepository.findByIdOrThrow(crossroadId);
+        String crossroadApiId = crossroad.getCrossroadApiId();
+
+        List<CrossroadStateApiResponse> apiResponses =
+            crossroadProvider.requestCrossroadStateApi(crossroadApiId);
+
+        if (apiResponses.isEmpty()) {
+            throw new BusinessException(CrossroadErrorCode.CROSSROAD_API_REQUEST_FAILED);
+        }
+
+        CrossroadStateResponse response = CrossroadMapper.INSTANCE.toResponse(
+            apiResponses.get(0), crossroadId
+        );
+        putStateCache(crossroadId, response);
+        return response;
     }
 
     public List<CrossroadApiResponse> getAllMarkers() {
@@ -60,5 +86,22 @@ public class CrossroadService {
         }
 
         return responseList;
+    }
+
+    private void putStateCache(Long crossroadId, CrossroadStateResponse response) {
+        ValueOperations<Object, Object> operations = redisTemplate.opsForValue();
+
+        int minTimeLeft = response.minTimeLeft();
+        minTimeLeft *= 100; // 1/10초 단위를 1/1000(ms)로 변환
+
+        operations.set(
+            STATE_PREFIX + crossroadId, response,
+            minTimeLeft, TimeUnit.MILLISECONDS
+        );
+    }
+
+    private CrossroadStateResponse getStateCache(Long crossroadId) {
+        ValueOperations<Object, Object> operations = redisTemplate.opsForValue();
+        return (CrossroadStateResponse) operations.get(STATE_PREFIX + crossroadId);
     }
 }
