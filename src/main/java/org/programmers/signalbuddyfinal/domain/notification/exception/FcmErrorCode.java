ea22a923268dd3ee226a257b1f4748259ae28488
DongminL <milkskfk12@gmail.com>
2025-03-05 23:19:11 +0900

    feat: FCM 알림을 요청한다

---
+++ src/main/java/org/programmers/signalbuddyfinal/domain/notification/exception/FcmErrorCode.java
@@ -0,0 +1,20 @@
+package org.programmers.signalbuddyfinal.domain.notification.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.programmers.signalbuddyfinal.global.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum FcmErrorCode implements ErrorCode {
+
+    FCM_SEND_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "19000", "FCM에 메시지 전송이 실패했습니다."),
+    FCM_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "19001", "해당 FCM Token을 찾지 못했습니다."),
+    ALREADY_EXISTED_TOKEN(HttpStatus.CONFLICT, "19002", "이미 해당 사용자에 대한 FCM Token이 존재합니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
