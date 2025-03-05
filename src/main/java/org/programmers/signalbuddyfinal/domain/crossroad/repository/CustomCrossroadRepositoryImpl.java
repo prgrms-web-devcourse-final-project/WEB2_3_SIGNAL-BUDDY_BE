8c28ea98a525c2cd8801b864e0bc24d7b8350677
DongjinKim <82895809+Dongjin0224@users.noreply.github.com>
2025-03-05 09:29:44 +0900

    [Feature] 소켓 통신 기능 구현 (#121)
    * feat: 사용자 위치 정보로 주변 교차로 조회 기능 구현
    
    * test: 교차로 WebSocket 기능 테스트
    
    * fix: 닫는 중괄호 추가

--- src/main/java/org/programmers/signalbuddyfinal/domain/crossroad/repository/CustomCrossroadRepositoryImpl.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/crossroad/repository/CustomCrossroadRepositoryImpl.java
@@ -1,47 +1,56 @@
 package org.programmers.signalbuddyfinal.domain.crossroad.repository;
 
+import static org.programmers.signalbuddyfinal.domain.crossroad.entity.QCrossroad.crossroad;
+
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
-import org.programmers.signalbuddyfinal.domain.crossroad.dto.CrossroadApiResponse;
-import org.programmers.signalbuddyfinal.domain.crossroad.entity.Crossroad;
-import org.programmers.signalbuddyfinal.domain.crossroad.entity.QCrossroad;
-import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.programmers.signalbuddyfinal.domain.crossroad.dto.CrossroadApiResponse;
+import org.programmers.signalbuddyfinal.domain.crossroad.dto.CrossroadResponse;
+import org.programmers.signalbuddyfinal.domain.crossroad.entity.Crossroad;
+import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class CustomCrossroadRepositoryImpl implements CustomCrossroadRepository {
 
-    private static final QCrossroad qCrossroad = QCrossroad.crossroad;
-
     private final JPAQueryFactory jqf;
 
     @Override
     public List<CrossroadApiResponse> findNearByCrossroads(double latitude, double longitude) {
-
         List<CrossroadApiResponse> res = new ArrayList<>();
         // ST_Distance_Sphere SQL 함수 사용
         String distanceExpression = "ST_Distance_Sphere(POINT({0}, {1}), coordinate)";
 
-        List<Crossroad> nearCrossroads = jqf.selectFrom(qCrossroad)
-                .where(Expressions.numberTemplate(Double.class, distanceExpression, longitude, latitude).loe(80)) // 거리 조건
-                .fetch();
+        List<Crossroad> nearCrossroads = jqf.selectFrom(crossroad).where(
+                Expressions.numberTemplate(Double.class, distanceExpression, longitude, latitude)
+                    .loe(80)) // 거리 조건
+            .fetch();
 
-        for(Crossroad near : nearCrossroads){
+        for (Crossroad near : nearCrossroads) {
             res.add(new CrossroadApiResponse(near));
         }
 
         return res;
     }
 
-/*
-    SELECT station,
-    ST_Distance_Sphere(@location, POINT(longitude, latitude) AS distance
-    FROM Subway
-    WHERE station = "신촌역";
-*/
 
+    @Override
+    public List<CrossroadResponse> findNearestCrossroads(double lat, double lng, int radius) {
+        return jqf.select(Projections.fields(CrossroadResponse.class, crossroad.crossroadId,
+            crossroad.crossroadApiId, crossroad.name,
+            Expressions.numberTemplate(Double.class, "ST_Y({0})", crossroad.coordinate).as("lat"),
+            Expressions.numberTemplate(Double.class, "ST_X({0})", crossroad.coordinate).as("lng"),
+            crossroad.status,
+            Expressions.numberTemplate(Double.class, "ST_DISTANCE_SPHERE(POINT({0}, {1}), {2})",
+                lng, lat, crossroad.coordinate).as("distance"))).from(crossroad).where(
+            Expressions.numberTemplate(Double.class, "ST_DISTANCE_SPHERE(POINT({0}, {1}), {2})",
+                lng, lat, crossroad.coordinate).loe(radius) // 반경 내 필터링
+        ).orderBy(
+            Expressions.numberTemplate(Double.class, "ST_DISTANCE_SPHERE(POINT({0}, {1}), {2})",
+                lng, lat, crossroad.coordinate).asc()).fetch();
+    }
 }
