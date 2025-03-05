368f297b0d6c980e47eb0191208a908d6da2fc83
zzuharchive__ <145355985+zzuharchive@users.noreply.github.com>
2025-03-05 10:17:12 +0900

    [Refactor] reissue액세스토큰 검증 추가 (#123)
    * refactor: reissue 액세스 토큰 검증 추가
    
    * refactor: 회원가입 시 탈퇴 사용자 체크
    
    * test: 테스트 코드 수정

--- src/main/java/org/programmers/signalbuddyfinal/domain/auth/controller/AuthController.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/auth/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,8 +32,10 @@ public class AuthController {
     }
 
     @PostMapping("/reissue")
-    public ResponseEntity<ApiResponse<Object>> reissue(@CookieValue(name = "refresh-token") String refreshToken){
-        return authService.reissue(refreshToken);
+    public ResponseEntity<ApiResponse<Object>> reissue(
+        @CookieValue(name = "refresh-token") String refreshToken,
+        @RequestHeader("Authorization") String accessToken) {
+        return authService.reissue(refreshToken, accessToken);
     }
 
     @PostMapping("/auth-code")
