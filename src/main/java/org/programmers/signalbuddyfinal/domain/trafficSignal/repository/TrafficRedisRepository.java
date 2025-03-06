feabe091f381c1e5a378d2ed424dd8311367f4f7
Byungil Oh <dhquddlf5@gmail.com>
2025-03-06 18:28:55 +0900

    feat: 신호등 위치정보 캐싱처리 및 id로 호출

---
+++ src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/repository/TrafficRedisRepository.java
@@ -0,0 +1,37 @@
+package org.programmers.signalbuddyfinal.domain.trafficSignal.repository;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.programmers.signalbuddyfinal.domain.trafficSignal.dto.TrafficResponse;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+@Repository
+@RequiredArgsConstructor
+public class TrafficRedisRepository {
+
+    private final RedisTemplate<Object, Object> redisTemplate;
+    private ValueOperations<Object, Object> operations;
+
+    private static final String KEY_PREFIX = "traffic:";
+
+    @PostConstruct
+    private void init() {
+        this.operations = redisTemplate.opsForValue();
+    }
+
+    public void save(TrafficResponse trafficResponse) {
+        operations.set(
+                KEY_PREFIX + trafficResponse.getSerialNumber(), trafficResponse,
+                Duration.ofMinutes(5)
+        );
+    }
+
+    public TrafficResponse findById(String id) {
+        return (TrafficResponse) operations.get(id);
+    }
+
+}
