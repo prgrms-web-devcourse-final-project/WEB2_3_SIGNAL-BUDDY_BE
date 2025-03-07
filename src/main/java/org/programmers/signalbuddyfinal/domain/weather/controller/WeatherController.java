95cd5ff9eb30eaeba89cd422a6197f7f7ba88149
dongJ <alalal2234@gmail.com>
2025-03-07 16:48:13 +0900

    fix: 컨벤션 적용
    - 임포트, 포맷터 적용

--- src/main/java/org/programmers/signalbuddyfinal/domain/weather/controller/WeatherController.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/weather/controller/WeatherController.java
@@ -1,10 +1,7 @@
 package org.programmers.signalbuddyfinal.domain.weather.controller;
 
 import java.io.IOException;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.programmers.signalbuddyfinal.domain.weather.dto.Weather;
-import org.programmers.signalbuddyfinal.domain.weather.dto.WeatherResponse;
 import org.programmers.signalbuddyfinal.domain.weather.service.WeatherService;
 import org.programmers.signalbuddyfinal.global.response.ApiResponse;
 import org.springframework.http.MediaType;
@@ -12,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -24,13 +20,15 @@ public class WeatherController {
     private final WeatherService weatherService;
 
     @PostMapping("/api/weather")
-    public ResponseEntity<ApiResponse<String>> saveExcel(@RequestBody String filePath) throws IOException {
+    public ResponseEntity<ApiResponse<String>> saveExcel(@RequestBody String filePath)
+        throws IOException {
         weatherService.saveExcel(filePath);
         return ResponseEntity.ok(ApiResponse.createSuccess("success"));
     }
 
     @GetMapping(value = "/sse/weather", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public ResponseEntity<SseEmitter> subscribeToWeather(@RequestParam double lat, @RequestParam double lng) {
+    public ResponseEntity<SseEmitter> subscribeToWeather(@RequestParam double lat,
+        @RequestParam double lng) {
         return ResponseEntity.ok(weatherService.subscribeToWeather(lat, lng));
     }
 
