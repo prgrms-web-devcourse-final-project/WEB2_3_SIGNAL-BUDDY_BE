b9b896dc0b178cfd1379a6778fbd2ffb1ed1c7ca
Byungil Oh <dhquddlf5@gmail.com>
2025-03-09 00:50:12 +0900

    chore: 사용하지 않는 코드 정리

--- src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/repository/CustomTrafficRepository.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/trafficSignal/repository/CustomTrafficRepository.java
@@ -1,7 +1,6 @@
 package org.programmers.signalbuddyfinal.domain.trafficSignal.repository;
 
 import org.programmers.signalbuddyfinal.domain.trafficSignal.dto.TrafficResponse;
-
 import java.util.List;
 
 public interface CustomTrafficRepository {
