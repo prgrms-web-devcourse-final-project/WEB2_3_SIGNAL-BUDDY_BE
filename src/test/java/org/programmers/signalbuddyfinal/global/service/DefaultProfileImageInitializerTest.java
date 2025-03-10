fa0968a440fdb95e2ed6b6606ad6b9e12428430c
DongjinKim <82895809+Dongjin0224@users.noreply.github.com>
2025-03-10 14:47:08 +0900

    [Refactor] 사용자 기본 프로필 이미지 변경 (#181)
    * refactor: 사용자 기본 프로필 이미지 변경
    
    * test: 사용자 프로필 이미지 변경 테스트 코드

--- src/test/java/org/programmers/signalbuddyfinal/global/service/DefaultProfileImageInitializerTest.java
+++ src/test/java/org/programmers/signalbuddyfinal/global/service/DefaultProfileImageInitializerTest.java
@@ -21,6 +21,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 import software.amazon.awssdk.awscore.exception.AwsErrorDetails;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
 import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -33,7 +34,10 @@ class DefaultProfileImageInitializerTest {
     private S3Client s3Client;
 
     @Mock
-    private Resource defaultImageResource;
+    private Resource oldProfileImage;
+
+    @Mock
+    private Resource newProfileImage;
 
     @InjectMocks
     private DefaultProfileImageInitializer initializer;
@@ -41,16 +45,18 @@ class DefaultProfileImageInitializerTest {
     @BeforeEach
     public void setUp() {
         ReflectionTestUtils.setField(initializer, "bucket", "test-bucket");
-        ReflectionTestUtils.setField(initializer, "defaultImageResource", defaultImageResource);
+        ReflectionTestUtils.setField(initializer, "oldProfileImage", oldProfileImage);
+        ReflectionTestUtils.setField(initializer, "newProfileImage", newProfileImage);
         ReflectionTestUtils.setField(initializer, "key", "key");
         ReflectionTestUtils.setField(initializer, "memberDir", "memberDir");
     }
 
     @DisplayName("S3에 기본 이미지가 존재하는 경우 업로드 실행되지 않아야 함")
     @Test
-    public void initDefaultImageWhenImageExists() {
-        HeadObjectResponse dummyResponse = HeadObjectResponse.builder().build();
+    public void initDefaultImageWhenImageExists() throws IOException {
+        HeadObjectResponse dummyResponse = HeadObjectResponse.builder().contentLength(100L).build();
         when(s3Client.headObject(any(HeadObjectRequest.class))).thenReturn(dummyResponse);
+        when(newProfileImage.contentLength()).thenReturn(100L);
 
         initializer.initDefaultProfileImage();
 
@@ -67,11 +73,32 @@ class DefaultProfileImageInitializerTest {
 
         byte[] dummyData = "dummy image data".getBytes();
         InputStream dummyInputStream = new ByteArrayInputStream(dummyData);
-        when(defaultImageResource.getInputStream()).thenReturn(dummyInputStream);
-        when(defaultImageResource.contentLength()).thenReturn((long) dummyData.length);
+        when(newProfileImage.getInputStream()).thenReturn(dummyInputStream);
+        when(newProfileImage.contentLength()).thenReturn((long) dummyData.length);
 
         initializer.initDefaultProfileImage();
 
+        verify(s3Client, times(1)).putObject(any(PutObjectRequest.class), any(RequestBody.class));
+    }
+
+    @DisplayName("S3에 저장된 이미지가 로컬에 저장된 이미지와 다르다면 삭제 후 업로드 실행")
+    @Test
+    public void initDefaultImageWhenImageDifferent() throws IOException {
+        // S3에 있는 파일 크기와 로컬 파일 크기가 다르게 설정 (삭제 유도)
+        when(newProfileImage.contentLength()).thenReturn(100L);
+        HeadObjectResponse dummyResponse = HeadObjectResponse.builder().contentLength(50L).build();
+        when(s3Client.headObject(any(HeadObjectRequest.class))).thenReturn(dummyResponse);
+
+        byte[] dummyData = "dummy image data".getBytes();
+        InputStream dummyInputStream = new ByteArrayInputStream(dummyData);
+        when(newProfileImage.getInputStream()).thenReturn(dummyInputStream);
+
+        initializer.initDefaultProfileImage();
+
+        // 먼저 deleteObject()가 실행되었는지 검증
+        verify(s3Client, times(1)).deleteObject(any(DeleteObjectRequest.class));
+
+        // 그리고 putObject()가 실행되었는지 검증
         verify(s3Client, times(1)).putObject(any(PutObjectRequest.class), any(RequestBody.class));
     }
 }
