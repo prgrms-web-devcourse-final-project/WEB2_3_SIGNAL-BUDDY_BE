b50c8d38106d6024f9adbf8ff4a3453a40a382a6
Byungil Oh <dhquddlf5@gmail.com>
2025-04-30 03:08:24 +0900

    test: TrafficCsvService 테스트 수정 및 작성

--- src/test/java/org/programmers/signalbuddyfinal/domain/trafficSignal/service/TrafficCsvServiceTest.java
+++ src/test/java/org/programmers/signalbuddyfinal/domain/trafficSignal/service/TrafficCsvServiceTest.java
@@ -1,46 +1,66 @@
-package org.programmers.signalbuddyfinal.domain.traffic.service;
+package org.programmers.signalbuddyfinal.domain.trafficSignal.service;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import java.net.URL;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.locationtech.jts.io.WKBWriter;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.programmers.signalbuddyfinal.domain.trafficSignal.dto.TrafficFileResponse;
-import org.programmers.signalbuddyfinal.domain.trafficSignal.repository.TrafficRepository;
-import org.programmers.signalbuddyfinal.domain.trafficSignal.service.TrafficCsvService;
 import org.programmers.signalbuddyfinal.global.support.ServiceTest;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.*;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 
 @Transactional
 public class TrafficCsvServiceTest extends ServiceTest {
 
-    @Autowired
-    private TrafficRepository trafficRepository;
-
-    @Autowired
+    @InjectMocks
     private TrafficCsvService trafficCsvService;
 
+    @Mock
+    private NamedParameterJdbcTemplate namedJdbcTemplate;
+
     private File testCsvFile;
+    private String fileName;
 
     @BeforeEach
-    void setUp() throws URISyntaxException, NullPointerException {
-        testCsvFile = new File(getClass()
-                .getClassLoader()
-                .getResource("static/file/seoul_traffic_light_test.csv")
-                .toURI());
+    void setUp() throws URISyntaxException {
+
+        fileName = "seoul_traffic_light.csv";
+
+        URL resource = getClass().getClassLoader().getResource("response/"+fileName);
+        assertThat(resource).isNotNull();
+        testCsvFile = new File(resource.toURI());
     }
 
     @Test
-    @DisplayName("데이터 랜더 처리 검증")
+    void csvSaveMethodTest(){
+        doReturn(new int[]{1, 1})
+            .when(namedJdbcTemplate)
+            .batchUpdate(anyString(), any(MapSqlParameterSource[].class));
+
+        trafficCsvService.saveCsvData(fileName);
+
+        verify(namedJdbcTemplate, atLeastOnce()).batchUpdate(anyString(), any(MapSqlParameterSource[].class));
+    }
+
+    @Test
+    @DisplayName("데이터 리더 처리 검증 및 메소드 호출")
     void csvParsingTest() throws IOException {
         // given
         Reader reader = new BufferedReader(new InputStreamReader(new FileInputStream(testCsvFile)));
@@ -72,7 +92,7 @@ public class TrafficCsvServiceTest extends ServiceTest {
     void csvParsingDtoTest() {
         // given
         String csvContent = """
-                serial, district, signalType, lat, lng, address
+                serial,district,signalType,lat,lng,address
                 123, 서울, 교차로, 37.5665, 126.9780, 서울시 중구
                 124, 부산, 신호등, 35.179, 129.0756, 부산시 해운대구
                 """;
@@ -94,18 +114,4 @@ public class TrafficCsvServiceTest extends ServiceTest {
 
     }
 
-
-    @Test
-    @DisplayName("보행등 정보 저장 검증")
-    void saveTrafficInfo() throws IOException {
-
-        String fileName = testCsvFile.getName();
-
-        //when
-        trafficCsvService.saveCsvData(fileName);
-
-        //then
-        assertThat(trafficRepository.count()).isGreaterThan(1L);
-
-    }
 }
