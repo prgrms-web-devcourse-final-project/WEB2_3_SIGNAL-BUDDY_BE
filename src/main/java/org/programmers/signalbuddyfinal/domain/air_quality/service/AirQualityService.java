fd3e6b7842d69ca54325ae10007dbbaaeb849e03
seohyeonmen <iacsbe36@gmail.com>
2025-04-30 14:58:14 +0900

    refactor: 캐시 데이터 확인 로직 분리

--- src/main/java/org/programmers/signalbuddyfinal/domain/air_quality/service/AirQualityService.java
+++ src/main/java/org/programmers/signalbuddyfinal/domain/air_quality/service/AirQualityService.java
@@ -2,6 +2,8 @@ package org.programmers.signalbuddyfinal.domain.air_quality.service;
 
 import java.time.Duration;
 import java.util.Optional;
+
+import io.jsonwebtoken.security.Jwks;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.programmers.signalbuddyfinal.domain.air_quality.dto.AirQuality;
@@ -11,6 +13,8 @@ import org.programmers.signalbuddyfinal.domain.air_quality.exception.AirQualityE
 import org.programmers.signalbuddyfinal.global.exception.BusinessException;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
 
 @Slf4j
 @Service
@@ -23,28 +27,28 @@ public class AirQualityService {
     private static final String key = "air-quality: ";
     private static final Duration TTL = Duration.ofHours(2);
 
-    public AirQualityResponse getAirQuality() {
-        CachedAirQuality cached = (CachedAirQuality) redisTemplate.opsForValue().get(key);
-        if (cached != null && cached.isFresh()) {
-            return cached.getData();
-        }
-        return updateAriQuality();
+    public Optional<AirQualityResponse> getAirQuality() {
+
+        Optional<AirQualityResponse> result = getCachedAirQuality();
+
+        return result.isPresent() ? result : updateAriQuality();
+
     }
 
-    public AirQualityResponse updateAriQuality() {
+    public Optional<AirQualityResponse> updateAriQuality() {
         Optional<AirQuality> airQuality = airQualityProvider.getAirQuality();
 
         // 응답 성공
         if (airQuality.isPresent()) {
             AirQualityResponse response = createResponse(airQuality);
             saveToCache(response, true);
-            return response;
+            return Optional.ofNullable(response);
         } else {
             // 응답 실패
             CachedAirQuality previous = (CachedAirQuality) redisTemplate.opsForValue().get(key);
             if (previous != null) {
                 saveToCache(previous.getData(), false);
-                return previous.getData();
+                return Optional.ofNullable(previous.getData());
             }
             throw new BusinessException(AirQualityErrorCode.AIR_QUALITY_SERVICE_UNAVAILABLE);
         }
@@ -52,13 +56,22 @@ public class AirQualityService {
 
     private AirQualityResponse createResponse(Optional<AirQuality> airQuality) {
         return AirQualityResponse.builder()
-            .grade(airQuality.get().getRow().get(0).getGrade())
-            .pm25(airQuality.get().getRow().get(0).getPm25())
-            .pm10(airQuality.get().getRow().get(0).getPm10())
-            .build();
+                .grade(airQuality.get().getRow().get(0).getGrade())
+                .pm25(airQuality.get().getRow().get(0).getPm25())
+                .pm10(airQuality.get().getRow().get(0).getPm10())
+                .build();
     }
 
     private void saveToCache(AirQualityResponse airQualityResponse, boolean fresh) {
         redisTemplate.opsForValue().set(key, new CachedAirQuality(airQualityResponse, fresh), TTL);
     }
+
+    private Optional<AirQualityResponse> getCachedAirQuality() {
+        CachedAirQuality cached = (CachedAirQuality) redisTemplate.opsForValue().get(key);
+        if (cached != null && cached.isFresh()) {
+            return Optional.ofNullable(cached.getData());
+        }
+        return Optional.empty();
+    }
+
 }
