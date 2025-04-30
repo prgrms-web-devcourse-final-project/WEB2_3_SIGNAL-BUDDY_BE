6816993dfd325ad67ace0de520260c3f0f6bf68f
DongminL <milkskfk12@gmail.com>
2025-05-01 01:12:45 +0900

    refactor: 좋아요 캐싱 로직 분리

---
+++ src/main/java/org/programmers/signalbuddyfinal/domain/like/service/LikeCacheService.java
@@ -0,0 +1,42 @@
+package org.programmers.signalbuddyfinal.domain.like.service;
+
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.programmers.signalbuddyfinal.domain.like.dto.LikeRequestType;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LikeCacheService {
+
+    private final StringRedisTemplate redisTemplate;
+
+    private static final String LIKE_KEY_PREFIX = "like:";
+
+    public void addLike(String key) {
+        redisTemplate.opsForValue()
+            .set(key, LikeRequestType.ADD.name(), 3L, TimeUnit.MINUTES);
+    }
+
+    public void cancelLike(String key) {
+        redisTemplate.opsForValue()
+            .set(key, LikeRequestType.CANCEL.name(), 3L, TimeUnit.MINUTES);
+    }
+
+    public boolean exists(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+    public String getLikeType(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public static String generateKey(Long feedbackId, Long memberId) {
+        return LIKE_KEY_PREFIX + feedbackId + ":" + memberId;
+    }
+}
